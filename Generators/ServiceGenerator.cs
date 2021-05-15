0f92274823056d47b4b5fa4e1c203bd4d08e404f
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-05-15 17:58:37 +0800

    add code generator, but it does not work !!

---
+++ Generators/ServiceGenerator.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generators
+{
+    //DOTO:  it does not work!
+    [Generator]
+    public class ServiceGenerator : ISourceGenerator
+    {
+        private const string code = @"
+namespace HelloGenerated
+{
+    public class HelloGenerator
+    {
+        public static void Test() => System.Console.WriteLine(""Hello Generator"");
+    }
+}
+";
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            //context.RegisterForPostInitialization(post =>
+            //{
+            //    post.AddSource("ServiceGeneratorRegisterForPostInitialization", code);
+            //});
+            //context.RegisterForSyntaxNotifications(() => new PrivateSyntaxReceiver());
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var code = @"
+namespace HelloGenerated
+{
+    public class HelloGenerator
+    {
+        public static void Test() => System.Console.WriteLine(""Hello ServiceGenerator"");
+    }
+}";
+            context.AddSource(nameof(ServiceGenerator), code);
+        }
+    }
+}
+
+internal class PrivateSyntaxReceiver : ISyntaxReceiver
+{
+    public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+    {
+
+    }
+}
