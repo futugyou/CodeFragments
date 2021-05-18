0cd501ce7ac6b6328b4eb6133704c521aa0278ed
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-05-18 11:11:34 +0800

    add dapr demo one

---
+++ DaprFrontEnd/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Dapr.Client;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DaprFrontEnd.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DaprClient _daprClient;
+
+        public IndexModel(DaprClient daprClient)
+        {
+            _daprClient = daprClient ?? throw new ArgumentNullException(nameof(daprClient));
+        }
+
+        public async Task OnGet()
+        {
+            var forecasts = await _daprClient.InvokeMethodAsync<IEnumerable<WeatherForecast>>(
+                HttpMethod.Get,
+                "daprbackend",
+                "weatherforecast");
+
+            ViewData["WeatherForecastData"] = forecasts;
+        }
+    }
+}
---
+++ DaprFrontEnd/WeatherForecast.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DaprFrontEnd
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
