54703b7ec3054c7cda43513eb2d3515c85ea0e8a
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-06-03 11:00:06 +0800

    add quic demo ,not ready now

---
+++ Quic/QuicConnectionDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Quic;
+using System.Threading.Tasks;
+
+namespace QuicConnectionDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var options = new System.Net.Security.SslClientAuthenticationOptions();
+            options.RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
+            QuicConnection quic = new(System.Net.IPEndPoint.Parse("192.168.15.135:666"), options);
+
+            await quic.ConnectAsync();
+
+            QuicStream quicStream = await quic.AcceptStreamAsync();
+
+            await quicStream.WriteAsync(System.Text.Encoding.UTF8.GetBytes("12345"));
+
+            byte[] buffer;
+            int count = await quicStream.ReadAsync(buffer = new byte[1000]);
+            string str = System.Text.Encoding.UTF8.GetString(buffer, 0, count);
+
+            Console.WriteLine(str);
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Quic/QuicListener/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net.Quic;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace QuicListenerDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        { 
+            var options = new System.Net.Security.SslServerAuthenticationOptions();
+            options.CertificateRevocationCheckMode = X509RevocationMode.NoCheck;
+            options.ServerCertificate = new X509Certificate("x509.crt", "*******");
+            options.RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
+            options.ApplicationProtocols = new System.Collections.Generic.List<System.Net.Security.SslApplicationProtocol> { System.Net.Security.SslApplicationProtocol.Http2 };
+            QuicListener quicListener = new(System.Net.IPEndPoint.Parse("192.168.15.135:666"), options);
+
+            quicListener.Start();
+
+            QuicConnection quicConnection = await quicListener.AcceptConnectionAsync();
+
+            QuicStream quicStream = await quicConnection.AcceptStreamAsync();
+            await quicStream.WriteAsync(System.Text.Encoding.UTF8.GetBytes("67890"));
+
+            byte[] buffer;
+            int count = await quicStream.ReadAsync(buffer = new byte[1000]);
+            string str = System.Text.Encoding.UTF8.GetString(buffer, 0, count);
+
+            Console.WriteLine(str);
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
