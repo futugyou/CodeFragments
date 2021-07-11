fcb8920d2355c0b53a5c1c3533b7775ac0e749e4
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-07-11 18:34:37 +0800

    add neural netowrk project

---
+++ Algorithm/NeuralNetwork/Neuron.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NeuralNetwork
+{
+    public class Neuron
+    {
+        public Guid Id { get; set; }
+        public List<Synapse> InputSynapses { get; set; }
+        public List<Synapse> OutputSynapses { get; set; }
+
+        public double Bias { get; set; }
+        public double BiasDelta { get; set; }
+
+        public double Gradient { get; set; }
+
+        public double Value { get; set; }
+
+
+        public bool IsMirror { get; set; }
+        public bool IsCanonical { get; set; }
+    }
+}
---
+++ Algorithm/NeuralNetwork/Synapse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+    public class Synapse
+    {
+        public Guid Id { get; set; }
+        public Neuron InpurNeuron { get; set; }
+        public Neuron OutputNeuron { get; set; }
+        public double Weight { get; set; }
+        public double WeightDelta { get; set; }
+    }
+}
