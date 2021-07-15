6d1e54de66f328c2e7cd2d103f9b68efc0943a08
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-07-15 15:43:50 +0800

    update Daily Coding Problem

---
+++ Algorithm/DailyCodingProblem/D00933.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingProblem
+{
+    /// <summary>
+    /// The sequence [0, 1, ..., N] has been jumbled, 
+    /// and the only clue you have for its order is an array representing whether each number is larger or smaller than the last.
+    /// Given this information, reconstruct an array that is consistent with it. 
+    /// For example, given [None, +, +, -, +], you could return [1, 2, 3, 0, 4].
+    /// </summary>
+    public class D00933
+    {
+        public static void Exection()
+        {
+            string[] given = { "", "+", "+", "-", "-", "+" };
+            int[] nums = Enumerable.Range(0, given.Length).ToArray();
+            var lesscount = given.Count(p => p == "-");
+            int[] result = new int[nums.Length];
+            result[0] = nums[lesscount];
+            var lessindex = 0;
+            for (int i = 1; i < given.Length; i++)
+            {
+                if (given[i] == "+")
+                {
+                    lesscount++;
+                    result[i] = nums[lesscount];
+                }
+                else
+                {
+                    result[i] = nums[lessindex];
+                    lessindex++;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ,", result));
+        }
+    }
+}
--- Algorithm/DailyCodingProblem/Program.cs
+++ Algorithm/DailyCodingProblem/Program.cs
@@ -26,7 +26,7 @@ namespace DailyCodingProblem
 
             //D00927.Exection();
 
-            D00931.Exection();
+            D00933.Exection();
 
             Console.WriteLine("Hello World!");
         }
