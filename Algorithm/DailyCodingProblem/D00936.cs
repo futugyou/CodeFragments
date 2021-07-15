e5b1007910b0ba2e51694a5295d57f7c8d317fcc
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-07-15 17:20:45 +0800

    update Daily Coding Problem

---
+++ Algorithm/DailyCodingProblem/D00936.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingProblem
+{
+
+
+    /// <summary>
+    /// Given a binary search tree and a range [a, b] (inclusive), return the sum of the elements of the binary search tree within the range.
+    /// For example, given the following tree:
+    ///      5
+    ///     / \
+    ///    3   8
+    ///   / \ / \
+    ///  2  4 6  10
+    /// and the range[4, 9], return 23 (5 + 4 + 6 + 8).
+    /// </summary>
+    public class D00936
+    {
+        public class D00936Tree
+        {
+            public int Value { get; set; }
+            public D00936Tree Left { get; set; }
+            public D00936Tree Right { get; set; }
+        }
+        public static void Exection()
+        {
+            var tree = new D00936Tree
+            {
+                Value = 5,
+                Left = new D00936Tree
+                {
+                    Value = 3,
+                    Left = new D00936Tree { Value = 2, },
+                    Right = new D00936Tree { Value = 4, },
+                },
+                Right = new D00936Tree
+                {
+                    Value = 8,
+                    Left = new D00936Tree { Value = 6, },
+                    Right = new D00936Tree { Value = 10, },
+                },
+            };
+
+            var range = new int[2] { 4, 9 };
+            int sum = Exection(tree, range);
+            Console.WriteLine(sum);
+        }
+
+        private static int Exection(D00936Tree tree, int[] range)
+        {
+            if (tree == null)
+            {
+                return 0;
+            }
+            var left = Exection(tree.Left, range);
+            var right = Exection(tree.Right, range);
+
+            return left + right + (tree.Value >= range[0] && tree.Value <= range[1] ? tree.Value : 0);
+        }
+    }
+}
--- Algorithm/DailyCodingProblem/Program.cs
+++ Algorithm/DailyCodingProblem/Program.cs
@@ -26,7 +26,7 @@ namespace DailyCodingProblem
 
             //D00927.Exection();
 
-            D00935.Exection();
+            D00936.Exection();
 
             Console.WriteLine("Hello World!");
         }
