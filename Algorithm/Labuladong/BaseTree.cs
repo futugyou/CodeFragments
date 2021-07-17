b1dafc1ddea3eef3c68faebbff48efae650511a7
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-07-17 19:10:42 +0800

    add labuladong project

---
+++ Algorithm/Labuladong/BaseTree.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labuladong
+{
+    public class BaseTree
+    {
+        public static BaseTree DefaultTree = new BaseTree(1)
+        {
+            Left = new BaseTree(2)
+            {
+                Left = new BaseTree(4),
+                Right = new BaseTree(5)
+            },
+            Right = new BaseTree(3)
+            {
+                Left = new BaseTree(6),
+                Right = new BaseTree(7)
+            }
+        };
+        public int Value { get; set; }
+        public BaseTree(int n)
+        {
+            Value = n;
+        }
+        public BaseTree Left { get; set; }
+        public BaseTree Right { get; set; }
+    }
+}
---
+++ Algorithm/Labuladong/Code124.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labuladong
+{
+    public class Code124
+    {
+        private static int result = int.MinValue;
+        public static void Exection()
+        {
+            var tree = BaseTree.DefaultTree;
+            Maxpathsum(tree);
+            Console.WriteLine(result);
+        }
+
+        private static int Maxpathsum(BaseTree tree)
+        {
+            if (tree == null)
+            {
+                return 0;
+            }
+            var left = Math.Max(0, Maxpathsum(tree.Left));
+            var right = Math.Max(0, Maxpathsum(tree.Right));
+            result = Math.Max(result, tree.Value + left + right);
+            return Math.Max(left, right) + tree.Value;
+        }
+    }
+}
