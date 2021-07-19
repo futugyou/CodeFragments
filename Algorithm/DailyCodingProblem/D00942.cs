852e8642a1440df478e8607994d3272e9935fe11
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-07-19 09:23:59 +0800

    updtae Daily Coding Problem

---
+++ Algorithm/DailyCodingProblem/D00942.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingProblem
+{
+    /// <summary>
+    /// Given a string of parentheses, write a function to compute the minimum number of parentheses to be removed to make the string valid 
+    /// (i.e. each open parenthesis is eventually closed).
+    /// For example, given the string "()())()", you should return 1. Given the string ")(", you should return 2, since we must remove all of them.
+    /// </summary>
+    public class D00942
+    {
+        public static void Exection()
+        {
+            var given = ")(()))((";
+            int count = 0;
+            int total = 0;
+            for (int i = 0; i < given.Length; i++)
+            {
+                if (given[i] == '(')
+                {
+                    count++;
+                }
+                if (given[i] == ')')
+                {
+                    count--;
+                }
+                if (count < 0)
+                {
+                    total++;
+                    count = 0;
+                }
+            }
+            Console.WriteLine(total+count);
+        }
+    }
+}
--- Algorithm/DailyCodingProblem/Program.cs
+++ Algorithm/DailyCodingProblem/Program.cs
@@ -26,7 +26,7 @@ namespace DailyCodingProblem
 
             //D00927.Exection();
 
-            D00937.Exection();
+            D00942.Exection();
 
             Console.WriteLine("Hello World!");
         }
