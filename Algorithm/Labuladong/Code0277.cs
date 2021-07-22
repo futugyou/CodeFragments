16a8fa9c240bbdd298e53ef084e531bca41a12ec
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-07-22 11:01:56 +0800

    update labuladong

--- Algorithm/Labuladong/Code0277.cs
+++ Algorithm/Labuladong/Code0277.cs
@@ -16,9 +16,35 @@ namespace Labuladong
             {0, 0, 1, 0 },
             {0, 0, 1, 1 },
             };
-            var result = ExecOne(nums);
+            // var result = ExecOne(nums);
+            var result = ExecTwo(nums);
             Console.WriteLine(result);
             return result;
+        }
+
+        private static int ExecTwo(int[,] nums)
+        {
+            int n = (int)Math.Sqrt(nums.Length);
+            int cand = 0;
+            // 排除掉非名人
+            for (int other = 0; other < n; other++)
+            {
+                if (cand == other) continue;
+                if (nums[cand, other] == 1 || nums[other, cand] == 0)
+                {
+                    cand = other;
+                }
+            }
+            // 确认cand是否是名人
+            for (int other = 0; other < n; other++)
+            {
+                if (cand == other) continue;
+                if (nums[cand, other] == 1 || nums[other, cand] == 0)
+                {
+                    return -1;
+                }
+            }
+            return cand;
         }
 
         private static int ExecOne(int[,] nums)
