69a657b26320ede175198234e7cc04aee1870c9a
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-08-19 09:28:49 +0800

    add seed data

---
+++ Identity/IdentityCenter/Config.cs
@@ -0,0 +1,39 @@
+﻿
+using IdentityServer4.Models;
+
+namespace IdentityCenter;
+public static class Config
+{
+    public static IEnumerable<ApiScope> ApiScopes =>
+           new List<ApiScope>
+           {
+               new ApiScope("api1", "My API")
+           };
+
+    public static IEnumerable<Client> Clients =>
+        new List<Client>
+        {
+            new Client
+            {
+                ClientId = "client",
+                // no interactive user, use the clientid/secret for authentication
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                
+                // secret for authentication                
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+                
+                // scopes that client has access to
+                AllowedScopes = { "api1" }
+            }
+        };
+
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+        };
+}
--- Identity/IdentityCenter/Startup.cs
+++ Identity/IdentityCenter/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
@@ -74,12 +75,36 @@ namespace IdentityCenter
             var scopeFactory = app.ApplicationServices.GetService<IServiceScopeFactory>();
             if (scopeFactory != null)
             {
-                using (var serviceScope = scopeFactory.CreateScope())
-                {
-                    serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+                using var serviceScope = scopeFactory.CreateScope();
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
 
-                    var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
-                    context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.Clients)
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.IdentityResources)
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiScopes.Any())
+                {
+                    foreach (var resource in Config.ApiScopes)
+                    {
+                        context.ApiScopes.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
                 }
             }
         }
