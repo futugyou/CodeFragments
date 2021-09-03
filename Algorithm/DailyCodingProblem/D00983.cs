3730668ced58fee90219acc71a147a659e3e2a3f
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-09-03 10:12:40 +0800

    update Daily Coding Problem

---
+++ Algorithm/DailyCodingProblem/D00983.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingProblem
+{
+    /// <summary>
+    /// Given a binary tree, return all paths from the root to leaves.    ///
+    ///  For example, given the tree:
+    ///      1
+    ///     / \
+    ///    2   3
+    ///       / \
+    ///      4   5
+    /// Return[[1, 2], [1, 3, 4], [1, 3, 5]].
+    /// </summary>
+    public class D00983
+    {
+        public static void Exection()
+        {
+            var tree = new Tree(1) { Left = new Tree(2) { Left = new Tree(6), Right = new Tree(7) }, Right = new Tree(3) { Left = new Tree(4), Right = new Tree(5) } };
+            if (tree == null)
+            {
+                return;
+            }
+            var list = new List<int>();
+            Show(tree, list);
+        }
+
+        private static void Show(Tree tree, List<int> list)
+        {
+            if (tree == null)
+            {
+                Console.WriteLine(string.Join(" ", list));
+                return;
+            }
+
+            list.Add(tree.Value);
+            if (tree.Left == null && tree.Right == null)
+            {
+                Show(null, list);
+            }
+            else
+            {
+                Show(tree.Left, list);
+                Show(tree.Right, list);
+            }
+            list.RemoveAt(list.Count - 1);
+        }
+
+        class Tree
+        {
+            public Tree(int n) => Value = n;
+            public int Value { get; set; }
+            public Tree Left { get; set; }
+            public Tree Right { get; set; }
+        }
+    }
+}
--- Algorithm/DailyCodingProblem/Program.cs
+++ Algorithm/DailyCodingProblem/Program.cs
@@ -26,7 +26,7 @@ namespace DailyCodingProblem
 
             //D00927.Exection();
 
-            D00973.Exection();
+            D00983.Exection();
 
             Console.WriteLine("Hello World!");
         }
