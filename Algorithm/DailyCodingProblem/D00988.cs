30cbf6fe9b19bd30a9215cee5523bc5ce825f31e
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-09-10 09:23:50 +0800

    Update Daily Coding Problem

---
+++ Algorithm/DailyCodingProblem/D00988.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingProblem
+{
+    /// <summary>
+    /// Given a positive integer n, find the smallest number of squared integers which sum to n.
+    /// For example, given n = 13, return 2 since 13 = 32 + 22 = 9 + 4.
+    /// Given n = 27, return 3 since 27 = 32 + 32 + 32 = 9 + 9 + 9.
+    /// </summary>
+    public class D00988
+    {
+        public static List<List<int>> results = new List<List<int>>();
+        public static void Exection()
+        {
+            var n = 27;
+            var list = new List<int>();
+            DoExec(n, list);
+            foreach (var item in results)
+            {
+                Console.WriteLine(string.Join(",", item));
+            }
+        }
+
+        private static void DoExec(int n, List<int> list)
+        {
+            if (n == 0)
+            {
+                results.Add(list.ToArray().ToList());
+                return;
+            }
+            for (int i = (int)Math.Sqrt(n); i >= 1; i--)
+            {
+                if (n >= i * i)
+                {
+                    list.Add(i);
+                    DoExec(n - i * i, list);
+                    list.Remove(i);
+                }
+            }
+        }
+    }
+}
--- Algorithm/DailyCodingProblem/Program.cs
+++ Algorithm/DailyCodingProblem/Program.cs
@@ -26,7 +26,7 @@ namespace DailyCodingProblem
 
             //D00927.Exection();
 
-            D00990.Exection();
+            D00988.Exection();
 
             Console.WriteLine("Hello World!");
         }
