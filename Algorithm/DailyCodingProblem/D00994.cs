96de84a8318bb1dc21e7ce3b5399bedfe5ebb05f
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-09-14 09:50:59 +0800

    Update Daily Coding Problem

---
+++ Algorithm/DailyCodingProblem/D00994.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingProblem
+{
+    /// <summary>
+    /// Print the nodes in a binary tree level-wise. For example, the following should print 1, 2, 3, 4, 5.
+    ///    1
+    ///   / \
+    ///  2   3
+    ///     / \
+    ///    4   5
+    /// </summary>
+    public class D00994
+    {
+        public static void Exection()
+        {
+            var tree = new Tree(1)
+            {
+                Left = new Tree(2) { Right = new Tree(9) { Left = new Tree(8), Right = new Tree(-1) }, },
+                Right = new Tree(3)
+                {
+                    Left = new Tree(4),
+                    Right = new Tree(5)
+                },
+            };
+
+            DoExec(tree);
+        }
+
+        private static void DoExec(Tree tree)
+        {
+            var queue = new Queue<Tree>();
+            queue.Enqueue(tree);
+            while (queue.Count > 0)
+            {
+                var currCount = queue.Count;
+                for (int i = 0; i < currCount; i++)
+                {
+                    var curr = queue.Dequeue();
+                    Console.WriteLine(curr.Value);
+                    if (curr.Left != null)
+                    {
+                        queue.Enqueue(curr.Left);
+                    }
+                    if (curr.Right != null)
+                    {
+                        queue.Enqueue(curr.Right);
+                    }
+                }
+            }
+
+        }
+
+        private class Tree
+        {
+            public Tree(int v)
+            {
+                Value = v;
+            }
+            public int Value { get; private set; }
+            public Tree Left { get; set; }
+            public Tree Right { get; set; }
+        }
+    }
+}
--- Algorithm/DailyCodingProblem/Program.cs
+++ Algorithm/DailyCodingProblem/Program.cs
@@ -7,26 +7,16 @@ namespace DailyCodingProblem
         static void Main(string[] args)
         {
             //D00917 d00917 = new D00917();
-            //d00917.Partition(6);
-
-            //D00919.Exection();
-
-            //D00920.Exection();
+            //d00917.Partition(6); 
 
             //for (int i = 0; i < 4097; i++)
             //{
             //    Console.WriteLine(i + "  " + D00923.Exection(i));
             //}
 
-            //D00924.Exection(new int[] { 3, 7, 5, 6, 9 });
+            //D00924.Exection(new int[] { 3, 7, 5, 6, 9 }); 
 
-            //D00925.Exection();
-
-            //D00926.Exection();
-
-            //D00927.Exection();
-
-            D00988.Exection();
+            D00994.Exection();
 
             Console.WriteLine("Hello World!");
         }
