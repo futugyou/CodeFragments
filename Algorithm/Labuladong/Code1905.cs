a7d0313ab78322e5f405517412bff961700b588b
futugyou <17871902+futugyou@users.noreply.github.com>
2021-10-19 15:21:44 +0800

    add leetcode 1905
    add leetcode 1905

---
+++ Algorithm/Labuladong/Code1905.cs
@@ -0,0 +1,70 @@
+namespace Labuladong;
+public class Code1905
+{
+    public static void Exection()
+    {
+        int[,] grid =  {
+            {1, 1, 0, 1, 0 },
+            {1, 0, 0, 0, 0 },
+            {0, 0, 0, 0, 0 },
+            {1, 1, 0, 1, 1 },
+            };
+
+        int[,] grid2 =  {
+            {0, 1, 0, 1, 0 },
+            {1, 0, 0, 0, 0 },
+            {0, 0, 0, 0, 0 },
+            {1, 1, 0, 1, 1 },
+            };
+
+        int res = 0;
+        int m = grid.GetLength(0);
+        int n = grid.GetLength(1);
+        Console.WriteLine(m + " " + n);
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (grid[i, j] == 0 && grid2[i, j] == 1)
+                {
+                    Dfs(grid2, i, j);
+                }
+            }
+        }
+
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (grid2[i, j] == 1)
+                {
+                    res++;
+                    Console.WriteLine("  " + i + " " + j);
+                    Dfs(grid2, i, j);
+                }
+            }
+        }
+        Console.WriteLine(res);
+    }
+
+    public static void Dfs(int[,] grid, int i, int j)
+    {
+        int m = grid.GetLength(0);
+        int n = grid.GetLength(1);
+        if (i < 0 || j < 0 || i >= m || j >= n)
+        {
+            return;
+        }
+        if (grid[i, j] == 0)
+        {
+            return;
+        }
+        grid[i, j] = 0;
+        Dfs(grid, i + 1, j);
+        Dfs(grid, i, j + 1);
+        Dfs(grid, i - 1, j);
+        Dfs(grid, i, j - 1);
+    }
+}
--- Algorithm/Labuladong/Program.cs
+++ Algorithm/Labuladong/Program.cs
@@ -7,7 +7,7 @@ namespace Labuladong
         static void Main(string[] args)
         {
             //Code0277.FindCelebrity();
-            Code0695.Exection();
+            Code1905.Exection();
             Console.WriteLine("Hello World!");
         }
     }
