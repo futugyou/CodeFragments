bff6e3a2855467c5e650b5a461b65c00372b3b0a
futugyou <17871902+futugyou@users.noreply.github.com>
2021-10-27 13:17:19 +0800

    update leetcode
    update leetcode

---
+++ Algorithm/Labuladong/Code0785.cs
@@ -0,0 +1,50 @@
+namespace Labuladong;
+public class Code0785
+{
+    public static void Exection()
+    {
+        var graph = new int[][] {
+            new int[] { 1, 2, 3 },
+            new int[] { 0, 2 },
+            new int[] { 0, 1, 3 },
+            new int[] { 0, 2 },
+            };
+        int n = graph.Length;
+        color = new bool[n];
+        visited = new bool[n];
+        for (int i = 0; i < n; i++)
+        {
+            if (!visited[i])
+            {
+                DFS(graph, i);
+            }
+        }
+        Console.WriteLine(ok);
+    }
+    private static bool ok = true;
+    private static bool[] color;
+    private static bool[] visited;
+    private static void DFS(int[][] graph, int n)
+    {
+        if (!ok)
+        {
+            return;
+        }
+        visited[n] = true;
+        foreach (var w in graph[n])
+        {
+            if (!visited[w])
+            {
+                color[w] = !color[n];
+                DFS(graph, w);
+            }
+            else
+            {
+                if (color[n] == color[w])
+                {
+                    ok = false;
+                }
+            }
+        }
+    }
+}
--- Algorithm/Labuladong/Program.cs
+++ Algorithm/Labuladong/Program.cs
@@ -7,7 +7,7 @@ namespace Labuladong
         static void Main(string[] args)
         {
             //Code0277.FindCelebrity();
-            Code0669.Exection();
+            Code0785.Exection();
             Console.WriteLine("Hello World!");
         }
     }
