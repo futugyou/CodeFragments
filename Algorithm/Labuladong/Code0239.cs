32cb69e269f01cb83873bbcd2e9a59ef7ee8e3df
futugyou <17871902+futugyou@users.noreply.github.com>
2021-11-19 00:07:04 +0800

    Update Labuladong
    Update Labuladong

---
+++ Algorithm/Labuladong/Code0239.cs
@@ -0,0 +1,53 @@
+namespace Labuladong;
+public class Code0239
+{
+    public static void Exection()
+    {
+        var nums = new int[] { 1, 3, -1, -3, 5, 3, 6, 7 };
+        var k = 3;
+        int[] result = MaxInStep(nums, k);
+        Console.WriteLine(string.Join(",", result));
+    }
+
+    public static int[] MaxInStep(int[] nums, int k)
+    {
+        var queue = new MonotonicQueue();
+        var result = new List<int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            queue.Push(nums[i]);
+            if (i >= k - 1)
+            {
+                result.Add(queue.Max());
+                queue.Pop(nums[i - k + 1]);
+            }
+        }
+        return result.ToArray();
+    }
+
+    public class MonotonicQueue
+    {
+        private LinkedList<int> q = new LinkedList<int>();
+        public void Push(int n)
+        {
+            while (q.Any() && q.Last() < n)
+            {
+                q.RemoveLast();
+            }
+            q.AddLast(n);
+        }
+
+        public int Max()
+        {
+            return q.First();
+        }
+
+        public void Pop(int n)
+        {
+            if (n == q.First())
+            {
+                q.RemoveFirst();
+            }
+        }
+    }
+}
--- Algorithm/Labuladong/Program.cs
+++ Algorithm/Labuladong/Program.cs
@@ -7,7 +7,7 @@ namespace Labuladong
         static void Main(string[] args)
         {
             //Code0277.FindCelebrity();
-            Code0213.Exection();
+            Code0239.Exection();
             Console.WriteLine("Hello World!");
         }
     }
