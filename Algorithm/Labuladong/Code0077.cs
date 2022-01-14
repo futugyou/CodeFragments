7648f66035f2925f1492854ee6cdc723a0a073bc
futugyou <17871902+futugyou@users.noreply.github.com>
2022-01-14 16:03:19 +0800

    Update Labuladong
    Update Labuladong

---
+++ Algorithm/Labuladong/Code0077.cs
@@ -0,0 +1,43 @@
+namespace Labuladong;
+
+public class Code0077
+{
+    public static void Exection()
+    {
+        var n = 4;
+        var k = 2;
+        Compose(n, k);
+        foreach (var item in result)
+        {
+            Console.WriteLine(string.Join(",", item));
+        }
+    }
+
+    private static void Compose(int n, int k)
+    {
+        var res = new List<int>();
+        BackTrack(res, 1, n, k);
+    }
+
+    private static void BackTrack(List<int> res, int start, int n, int k)
+    {
+        if (res.Count == k)
+        {
+            var t = new int[k];
+            res.CopyTo(0, t, 0, k);
+            result.Add(t.ToList());
+            return;
+        }
+        for (int i = start; i <= n; i++)
+        {
+            if (!res.Contains(i))
+            {
+                res.Add(i);
+                BackTrack(res, start + 1, n, k);
+                res.RemoveAt(res.Count - 1);
+            }
+        }
+    }
+
+    private static List<List<int>> result = new();
+}
--- Algorithm/Labuladong/Program.cs
+++ Algorithm/Labuladong/Program.cs
@@ -4,7 +4,7 @@ class Program
     static void Main(string[] args)
     {
         //Code0277.FindCelebrity();
-        Code0046.Exection();
+        Code0077.Exection();
         Console.WriteLine("Hello World!");
     }
 }
