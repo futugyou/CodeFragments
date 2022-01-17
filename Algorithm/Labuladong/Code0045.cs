6a396275d8a9396804c99221afeb982b3ab7ef32
futugyou <17871902+futugyou@users.noreply.github.com>
2022-01-17 21:24:56 +0800

    Update Labuladong
    Update Labuladong

--- Algorithm/Labuladong/Code0045.cs
+++ Algorithm/Labuladong/Code0045.cs
@@ -4,9 +4,30 @@ public class Code0045
 {
     public static void Exection()
     {
-        var nums = new int[] { 2, 3, 1, 1, 4 };
+        var nums = new int[] { 2, 2, 1, 1, 4 };
         int result = Jump(nums);
         Console.WriteLine(result);
+        result = Jump2(nums);
+        Console.WriteLine(result);
+    }
+
+    private static int Jump2(int[] nums)
+    {
+        var dp = new int[nums.Length];
+        Array.Fill(dp, nums.Length);
+        dp[0] = 0;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (i <= j + nums[j])
+                {
+                    dp[i] = Math.Min(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        Console.WriteLine(string.Join(",", dp));
+        return dp[nums.Length - 1];
     }
 
     private static int Jump(int[] nums)
