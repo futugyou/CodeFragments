91c83dd59418cc1711ab0e45a564c9f65790ceac
futugyou <17871902+futugyou@users.noreply.github.com>
2022-01-18 15:34:25 +0800

    fix bug
    fix bug

--- Algorithm/Labuladong/Code0322.cs
+++ Algorithm/Labuladong/Code0322.cs
@@ -1,38 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Labuladong
+﻿namespace Labuladong;
+public class Code0322
 {
-    public class Code0322
+    public static void Exection()
     {
-        public static int CoinChange(int[] coins, int amount)
+        var coins = new int[] { 1, 2, 5 };
+        var amount = 11;
+        int result = CoinChange(coins, amount);
+        Console.WriteLine(result);
+    }
+
+    public static int CoinChange(int[] coins, int amount)
+    {
+        if (amount == 0)
         {
-            if (amount == 0)
+            return -1;
+        }
+        // dp[i] : when amount = i, mix coin count is dp[i].
+        int[] dp = new int[amount + 1];
+        Array.Fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++)
+        {
+            foreach (var coin in coins)
             {
-                return -1;
-            }
-            // dp[i] : when amount = i, mix coin count is dp[i].
-            int[] dp = new int[coins.Length + 1];
-            for (int i = 0; i < dp.Length; i++)
-            {
-                dp[i] = amount + 1;
-            }
-            dp[0] = 0;
-            for (int i = 1; i <= amount; i++)
-            {
-                foreach (var coin in coins)
+                if (i >= coin)
                 {
-                    if (i < coin)
-                    {
-                        continue;
-                    }
                     dp[i] = Math.Min(dp[i], dp[i - coin] + 1);
                 }
             }
-            return dp[amount];
         }
+        return dp[amount];
     }
 }
--- Algorithm/Labuladong/Program.cs
+++ Algorithm/Labuladong/Program.cs
@@ -4,7 +4,7 @@ class Program
     static void Main(string[] args)
     {
         //Code0277.FindCelebrity();
-        Code0300.Exection();
+        Code0322.Exection();
         Console.WriteLine("Hello World!");
     }
 }
