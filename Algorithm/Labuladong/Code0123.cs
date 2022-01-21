c0ed35b52faf38de499669c31b495791acf13e24
futugyou <17871902+futugyou@users.noreply.github.com>
2022-01-21 16:52:07 +0800

    Update Labuladong
    Update Labuladong

--- Algorithm/Labuladong/Code0123.cs
+++ Algorithm/Labuladong/Code0123.cs
@@ -9,22 +9,21 @@ public class Code0123
         Console.WriteLine(res);
     }
 
-    // buy infinity, sell infinity, but sell have one day for CD.
+    // buy 2 times, sell 2 times
     private static int MaxProfit(int[] prices)
     {
         var n = prices.Length;
-        var dp = new int[n, 2];
-        dp[0, 1] = -prices[0];
-        dp[0, 0] = 0;
-
-        dp[1, 1] = Math.Max(dp[0, 1], -prices[1]);
-        dp[1, 0] = Math.Max(dp[0, 0], dp[1, 1] + prices[1]);
-
-        for (int i = 2; i < n; i++)
+        // day, times, buy/sell
+        var dp = new int[n, 2, 2];
+        dp[0, 0, 1] = -prices[0];
+        dp[0, 0, 0] = 0;
+        for (int i = 1; i < n; i++)
         {
-            dp[i, 1] = Math.Max(dp[i - 1, 1], dp[i - 2, 0] - prices[i]);
-            dp[i, 0] = Math.Max(dp[i - 1, 0], dp[i, 1] + prices[i]);
+            dp[i, 0, 1] = Math.Max(dp[i - 1, 0, 1], -prices[i]);
+            dp[i, 0, 0] = Math.Max(dp[i - 1, 0, 0], dp[i, 0, 1] + prices[i]);
+            dp[i, 1, 1] = Math.Max(dp[i - 1, 1, 1], dp[i - 1, 0, 0] - prices[i]);
+            dp[i, 1, 0] = Math.Max(dp[i - 1, 1, 0], dp[i, 1, 1] + prices[i]);
         }
-        return dp[n - 1, 0];
+        return dp[n - 1, 1, 0];
     }
 }
---
+++ Algorithm/Labuladong/Code0309.cs
@@ -0,0 +1,30 @@
+namespace Labuladong;
+
+public class Code0309
+{
+    public static void Exection()
+    {
+        var prices = new int[] { 7, 1, 5, 3, 7, 4 };
+        int res = MaxProfit(prices);
+        Console.WriteLine(res);
+    }
+
+    // buy infinity, sell infinity, but sell have one day for CD.
+    private static int MaxProfit(int[] prices)
+    {
+        var n = prices.Length;
+        var dp = new int[n, 2];
+        dp[0, 1] = -prices[0];
+        dp[0, 0] = 0;
+
+        dp[1, 1] = Math.Max(dp[0, 1], -prices[1]);
+        dp[1, 0] = Math.Max(dp[0, 0], dp[1, 1] + prices[1]);
+
+        for (int i = 2; i < n; i++)
+        {
+            dp[i, 1] = Math.Max(dp[i - 1, 1], dp[i - 2, 0] - prices[i]);
+            dp[i, 0] = Math.Max(dp[i - 1, 0], dp[i, 1] + prices[i]);
+        }
+        return dp[n - 1, 0];
+    }
+}
