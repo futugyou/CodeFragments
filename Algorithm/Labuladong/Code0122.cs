2784c5ebb8271e96f79d0d2d7aae720ba86d894a
futugyou <17871902+futugyou@users.noreply.github.com>
2022-01-21 16:06:05 +0800

    Update Labuladong
    Update Labuladong

---
+++ Algorithm/Labuladong/Code0122.cs
@@ -0,0 +1,25 @@
+namespace Labuladong;
+
+public class Code0122
+{
+    public static void Exection()
+    {
+        var prices = new int[] { 7, 1, 5, 3, 7, 4 };
+        int res = MaxProfit(prices);
+        Console.WriteLine(res);
+    }
+
+    private static int MaxProfit(int[] prices)
+    {
+        var n = prices.Length;
+        var dp = new int[n, 2];
+        dp[0, 1] = -prices[0];
+        dp[0, 0] = 0;
+        for (int i = 1; i < n; i++)
+        {
+            dp[i, 1] = Math.Max(dp[i - 1, 1], dp[i - 1, 0] - prices[i]);
+            dp[i, 0] = Math.Max(dp[i - 1, 0], dp[i, 1] + prices[i]);
+        }
+        return dp[n - 1, 0];
+    }
+}
--- Algorithm/Labuladong/Program.cs
+++ Algorithm/Labuladong/Program.cs
@@ -4,7 +4,7 @@ class Program
     static void Main(string[] args)
     {
         //Code0277.FindCelebrity();
-        Code0648.Exection();
+        Code0122.Exection();
         Console.WriteLine("Hello World!");
     }
 }
