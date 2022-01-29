422d100bbc8b62583c3ddf270832f1fde65b45c8
futugyou <17871902+futugyou@users.noreply.github.com>
2022-01-29 15:21:13 +0800

    Update Labuladong
    Update Labuladong

---
+++ Algorithm/Labuladong/Code0452.cs
@@ -0,0 +1,32 @@
+namespace Labuladong;
+public class Code0452
+{
+    public static void Exection()
+    {
+        var intervals = new int[][] {
+            new int[] { 10, 16 },
+            new int[] { 2, 8 },
+            new int[] { 1, 6 },
+            new int[] { 7, 12 },};
+
+        var result = GetInterval(intervals);
+        Console.WriteLine(result);
+    }
+
+    private static int GetInterval(int[][] intervals)
+    {
+        var list = new List<int[]>();
+        var tmp = intervals.ToList().OrderBy(o => o[0]).ThenByDescending(o => o[1]).ToList();
+        list.Add(tmp.First());
+        for (int i = 1; i < tmp.Count; i++)
+        {
+            var per = list.Last();
+            var curr = tmp[i];
+            if (curr[0] > per[1])
+            {
+                list.Add(curr);
+            }
+        }
+        return list.Count;
+    }
+}
--- Algorithm/Labuladong/Program.cs
+++ Algorithm/Labuladong/Program.cs
@@ -4,7 +4,7 @@ class Program
     static void Main(string[] args)
     {
         //Code0277.FindCelebrity();
-        Code0435.Exection();
+        Code0452.Exection();
         Console.WriteLine("Hello World!");
     }
 }
