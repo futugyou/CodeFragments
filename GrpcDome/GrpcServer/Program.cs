c8a91b9f2b9cf08684430893679a9c5fc9f9bdc5
futugyou <17871902+futugyou@users.noreply.github.com>
2022-02-02 19:57:23 +0800

    add grpc service dome
    add grpc service dome

---
+++ GrpcDome/GrpcServer/Program.cs
@@ -0,0 +1,25 @@
+using GrpcServer.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+// HTTP/2 over TLS is not supported on Windows versions earlier than Windows 10
+builder.WebHost.ConfigureKestrel(op =>
+{
+    op.ListenLocalhost(50001, a =>
+    {
+        a.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2;
+    });
+});
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGrpcService<FourTypeService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
---
+++ GrpcDome/GrpcServer/Services/FourTypeService.cs
@@ -0,0 +1,46 @@
+using Grpc.Core;
+using GrpcServer;
+
+namespace GrpcServer.Services;
+
+public class FourTypeService : FourType.FourTypeBase
+{
+    private readonly ILogger<FourTypeService> logger;
+    public FourTypeService(ILogger<FourTypeService> logger)
+    {
+        this.logger = logger;
+    }
+    public override Task<FourTypeResponse> UnaryCall(FourTypeRequest request, ServerCallContext context)
+    {
+        var userAgent = context.RequestHeaders.GetValue("user-agent");
+        logger.LogInformation("UnaryCall: user-agent is " + userAgent);
+        var response = new FourTypeResponse();
+        return Task.FromResult(response);
+    }
+    public override async Task StreamingFromServer(FourTypeRequest request, IServerStreamWriter<FourTypeResponse> responseStream, ServerCallContext context)
+    {
+        for (var i = 0; i < 5; i++)
+        {
+            await responseStream.WriteAsync(new FourTypeResponse());
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
+
+    public override async Task<FourTypeResponse> StreamingFromClient(IAsyncStreamReader<FourTypeRequest> requestStream, ServerCallContext context)
+    {
+        while (await requestStream.MoveNext())
+        {
+            var message = requestStream.Current;
+            logger.LogInformation("StreamingFromClient: message is " + message);
+        }
+        return new FourTypeResponse();
+    }
+
+    public override async Task StreamingBothWays(IAsyncStreamReader<FourTypeRequest> requestStream, IServerStreamWriter<FourTypeResponse> responseStream, ServerCallContext context)
+    {
+        await foreach (var message in requestStream.ReadAllAsync())
+        {
+            await responseStream.WriteAsync(new FourTypeResponse());
+        }
+    }
+}
