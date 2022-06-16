0ae93928d5e4684e96e72a2ce6bd687024cd3739
futugyou <17871902+futugyou@users.noreply.github.com>
2022-06-16 14:39:30 +0800

    add  elsa demo
    add  elsa demo

---
+++ Elsademo/ElsaCore.Server/Workflows/HelloWorldWorkflow.cs
@@ -0,0 +1,20 @@
+﻿using Elsa.Builders;
+using Elsa.Activities.Http;
+
+namespace ElsaCore.Server.Workflows;
+public class HelloWorldWorkflow : IWorkflow
+{
+    public void Build(IWorkflowBuilder builder)
+    {
+        builder.HttpEndpoint(setup =>
+        {
+            setup.WithMethod(HttpMethod.Get.Method).WithPath("/code/hello-world");
+        })
+            .Then<WriteHttpResponse>(setup =>
+            {
+                setup.WithContentType("text/html")
+                .WithContent("<h1>Hello World! </h1><p>这是通过代码实现的流程</p>")
+                .WithStatusCode(System.Net.HttpStatusCode.OK);
+            });
+    }
+}
