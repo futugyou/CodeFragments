e0ed6b902f356b538cda638e9f670704e2a560bd
futugyou <17871902+futugyou@users.noreply.github.com>
2022-07-31 15:49:10 +0800

    update quartz demo
    update quartz demo

--- ScheduleSystem/QuartzDemo/Controllers/DemoController.cs
+++ ScheduleSystem/QuartzDemo/Controllers/DemoController.cs
@@ -24,12 +24,73 @@ public class DemoController : ControllerBase
         var trigger = TriggerBuilder.Create()
             .WithIdentity("trigger1", "group1")
             .StartNow()
+            // second minute hour / forever forcount
             .WithSimpleSchedule(x => x.WithIntervalInSeconds(10).RepeatForever())
             .Build();
 
         var scheduler = await schedulerFactory.GetScheduler();
 
         await scheduler.ScheduleJob(job, trigger);
+        return "";
+    }
+
+    [HttpGet("daily")]
+    public async Task<string> Daily()
+    {
+        var job = JobBuilder.Create<HelloJob>().WithIdentity("job1", "group1").Build();
+        var trigger = TriggerBuilder.Create()
+            .WithIdentity("trigger1", "group1")
+            .StartNow()
+            // start end week
+            .WithDailyTimeIntervalSchedule(x => x.WithIntervalInSeconds(10))
+            .Build();
+
+        var scheduler = await schedulerFactory.GetScheduler();
+
+        await scheduler.ScheduleJob(job, trigger);
+        return "";
+    }
+
+    [HttpGet("cron")]
+    public async Task<string> Cron()
+    {
+        var job = JobBuilder.Create<HelloJob>().WithIdentity("job1", "group1").Build();
+        var trigger = TriggerBuilder.Create()
+            .WithIdentity("trigger1", "group1")
+            .StartNow()
+            .WithCronSchedule("0/15 * * * * ? ")
+            .Build();
+
+        var scheduler = await schedulerFactory.GetScheduler();
+
+        await scheduler.ScheduleJob(job, trigger);
+        return "";
+    }
+
+    [HttpGet("delete")]
+    public async Task<string> Delete()
+    {
+        var scheduler = await schedulerFactory.GetScheduler();
+        var jobkey = JobKey.Create("job1", "group1");
+        await scheduler.DeleteJob(jobkey);
+        return "";
+    }
+
+    [HttpGet("pause")]
+    public async Task<string> Pause()
+    {
+        var scheduler = await schedulerFactory.GetScheduler();
+        var jobkey = JobKey.Create("job1", "group1");
+        await scheduler.PauseJob(jobkey);
+        return "";
+    }
+
+    [HttpGet("resume")]
+    public async Task<string> Resume()
+    {
+        var scheduler = await schedulerFactory.GetScheduler();
+        var jobkey = JobKey.Create("job1", "group1");
+        await scheduler.ResumeJob(jobkey);
         return "";
     }
 }
