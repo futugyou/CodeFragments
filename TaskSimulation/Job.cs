376d411725a5fa13eb8931408c3ce07f6be8f680
futugyou <17871902+futugyou@users.noreply.github.com>
2022-09-20 10:53:29 +0800

    TEC-2 add job
    TEC-2 add job

---
+++ TaskSimulation/Job.cs
@@ -0,0 +1,26 @@
+﻿namespace TaskSimulation;
+
+public class Job
+{
+    private readonly Action _work;
+    public Job(Action work)
+    {
+        _work = work;
+    }
+    public JobStatus Status { get; internal set; }
+
+    internal protected virtual void Invoke()
+    {
+        Status = JobStatus.Running;
+        _work();
+        Status = JobStatus.Completed;
+    }
+}
+
+public enum JobStatus
+{
+    Created,
+    Scheduled,
+    Running,
+    Completed
+}
---
+++ TaskSimulation/readme.md
@@ -0,0 +1,1 @@
+https://mp.weixin.qq.com/s/h0L5QL_G8aVgDNaUcrs9YA
