608b112a3a659dcc114607cc3415c0089aecd6d1
futugyou <17871902+futugyou@users.noreply.github.com>
2022-12-05 11:12:32 +0800

    fix error
    fix error

---
+++ AspnetcoreEx/RedisExtensions/RedisConfigValidation.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace AspnetcoreEx.RedisExtensions;
+
+public class RedisConfigValidation : IValidateOptions<RedisOptions>
+{
+    public ValidateOptionsResult Validate(string? name, RedisOptions options)
+    {
+        return ValidateOptionsResult.Success;
+    }
+}
