4fc2ad37ed6d30c862ac5bb835fa23d639e88f08
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2022-12-14 06:25:44 +0000

    add  hosted service with IHostedService

---
+++ AspnetcoreEx/Extensions/ServiceHostedEx/PerformanceMetricsCollector.cs
@@ -0,0 +1,22 @@
+namespace AspnetcoreEx.Extensions;
+
+public sealed class PerformanceMetricsCollector : IHostedService
+{
+    private IDisposable? _scheduler;
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _scheduler = new Timer(Callback, null, TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(5));
+        return Task.CompletedTask;
+
+        static void Callback(object? state)
+        {
+            Console.WriteLine($"[{DateTimeOffset.Now}]{PerformanceMetrice.Create()}");
+        }
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _scheduler?.Dispose();
+        return Task.CompletedTask;
+    }
+}
