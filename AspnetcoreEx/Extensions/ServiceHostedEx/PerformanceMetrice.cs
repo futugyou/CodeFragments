c6dd249830df076e200a2231155c72f75ddfc886
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2022-12-14 06:12:48 +0000

    add PerformanceMetrice class

---
+++ AspnetcoreEx/Extensions/ServiceHostedEx/PerformanceMetrice.cs
@@ -0,0 +1,25 @@
+namespace AspnetcoreEx.Extensions;
+
+public class PerformanceMetrice
+{
+    private static readonly Random _random = new();
+
+    public int Processor { get; set; }
+    public long Memory { get; set; }
+    public long Network { get; set; }
+
+    public override string ToString()
+    {
+        return @$"CPU: {Processor * 100}%; Memory: {Memory / (1024 * 1024)}M; Network: {Network / (1024 * 1024)}M/s";
+    }
+
+    public static PerformanceMetrice Create()
+    {
+        return new()
+        {
+            Processor = _random.Next(1, 8),
+            Memory = _random.Next(10, 100) * 1024 * 1024,
+            Network = _random.Next(10, 100) * 1024 * 1024
+        };
+    }
+}
