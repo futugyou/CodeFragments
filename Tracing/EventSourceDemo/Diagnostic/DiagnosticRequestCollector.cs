7f77a23bdb71854d5aa15fc9fc276c71095433a5
futugyou <17871902+futugyou@users.noreply.github.com>
2022-12-16 11:46:16 +0800

    add more Microsoft.AspNetCore diagnostic
    add more Microsoft.AspNetCore diagnostic

--- Tracing/EventSourceDemo/Diagnostic/DiagnosticRequestCollector.cs
+++ Tracing/EventSourceDemo/Diagnostic/DiagnosticRequestCollector.cs
@@ -7,15 +7,27 @@ namespace EventSourceDemo.Diagnostic
     public sealed class DiagnosticRequestCollector
     {
         [DiagnosticName("Microsoft.AspNetCore.Hosting.BeginRequest")]
-        public void OnReveiveRequest(HttpContext httpContext, long timestamp)
+        public void OnRequestStart(HttpContext httpContext, long timestamp)
         {
-            Console.WriteLine($"Reveice request url:{httpContext.Request.ToString()} ; Timestamp:{timestamp}");
+            var request = httpContext.Request;
+            Console.WriteLine($"Request starting :{request.Protocol} {request.Method} {request.Scheme}://{request.Host}{request.PathBase}{request.Path}; Timestamp:{timestamp}");
+            httpContext.Items["StartingTimestamp"] = timestamp;
         }
 
         [DiagnosticName("Microsoft.AspNetCore.Hosting.EndRequest")]
-        public void OnSendReply(HttpContext httpContext, long currentTimestamp)
+        public void OnRequestEnd(HttpContext httpContext, long currentTimestamp)
+        {   
+            var startTimestamp = long.Parse(httpContext.Items["StartingTimestamp"]!.ToString());
+            var timestamoToTicks = TimeSpan.TicksPerSecond / (double)Stopwatch.Frequency;
+            var elapsed = new TimeSpan((long)(timestamoToTicks * (currentTimestamp - startTimestamp)));
+            Console.WriteLine($"send reply status code:{httpContext.Response.StatusCode} ; Request finished:{elapsed.TotalMilliseconds} ms");
+        }
+
+        [DiagnosticName("Microsoft.AspNetCore.Hosting.UnhandledException")]
+        public void OnException(HttpContext httpContext, long timestamp, Exception exception)
         {
-            Console.WriteLine($"send reply status code:{httpContext.Response.StatusCode} ; Timestamp:{currentTimestamp}");
+            OnRequestEnd(httpContext, timestamp);
+            Console.WriteLine($"{exception.Message} Type:{exception.GetType()} Stacktrace: {exception.StackTrace}");
         }
     }
 }
