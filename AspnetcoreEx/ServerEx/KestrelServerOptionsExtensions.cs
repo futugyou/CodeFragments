a0a847e11f2d7b7c22c26093e3062ed5320fe131
futugyou <17871902+futugyou@users.noreply.github.com>
2022-12-17 19:46:33 +0800

    add Mini Kestrel Server
    add Mini Kestrel Server

---
+++ AspnetcoreEx/ServerEx/KestrelServerOptionsExtensions.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.Reflection;
+using System.Collections.Generic;
+
+public static class KestrelServerOptionsExtensions
+{
+    public static IEnumerable<ListenOptions> GetListenOptions(this KestrelServerOptions options)
+    {
+        var property = typeof(KestrelServerOptions).GetProperty("ListenOptions", BindingFlags.NonPublic | BindingFlags.Instance);
+        return (IEnumerable<ListenOptions>)property!.GetValue(optsion);
+    }
+}
