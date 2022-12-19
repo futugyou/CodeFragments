0b86d01a7c5acb25c33690021113e00873cac387
futugyou <17871902+futugyou@users.noreply.github.com>
2022-12-19 12:10:02 +0800

    add BindAsync for point
    this mothed is also called by databind

--- AspnetcoreEx/RouteEx/PointForRoute.cs
+++ AspnetcoreEx/RouteEx/PointForRoute.cs
@@ -1,3 +1,6 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.Reflection;
 
 namespace AspnetcoreEx.RouteEx;
 
@@ -22,5 +25,26 @@ public class PointForRoute
         }
         point = null;
         return false;
+    }
+
+    public static ValueTask<PointForRoute?> BindAsync(HttpContext httpContext, ParameterInfo parameter)
+    {
+        PointForRoute? point = null;
+        var name = parameter.Name;
+
+        var value = httpContext.GetRouteData().Values.TryGetValue(name!, out var v) 
+            ? v 
+            : httpContext.Request.Query[name!].SingleOrDefault();
+        
+        if (value is string expression)
+        {
+            var split = expression.Trim('(',')').Split(',');
+            if (split.Length == 2 && int.TryParse(split[0], out var x) && int.TryParse(split[1], out var y))
+            {
+                point = new PointForRoute(x, y);
+            }
+        }
+        
+        return new ValueTask<PointForRoute?>(point);
     }
 }
