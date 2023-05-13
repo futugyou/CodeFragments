8b78c134cfb5e43d325e00ee4e9e00d13d4fcbdf
futugyou <17871902+futugyou@users.noreply.github.com>
2023-05-13 19:20:01 +0800

    create sl function step by step
    create sl function step by step

--- AspnetcoreEx/Controllers/SemanticKernelController.cs
+++ AspnetcoreEx/Controllers/SemanticKernelController.cs
@@ -1,69 +1,121 @@
 ﻿using AspnetcoreEx.SemanticKernel;
+using Microsoft.SemanticKernel.SemanticFunctions;
 
-namespace AspnetcoreEx.Controllers
+namespace AspnetcoreEx.Controllers;
+[Route("api/sk")]
+[ApiController]
+public class SemanticKernelController : ControllerBase
 {
-    [Route("api/sk")]
-    [ApiController]
-    public class SemanticKernelController : ControllerBase
+    private readonly IKernel kernel;
+    private readonly SemanticKernelOptions options;
+
+    public SemanticKernelController(IKernel kernel, IOptionsMonitor<SemanticKernelOptions> optionsMonitor)
     {
-        private readonly IKernel kernel;
-        private readonly SemanticKernelOptions options;
+        this.kernel = kernel;
+        this.options = optionsMonitor.CurrentValue;
+    }
 
-        public SemanticKernelController(IKernel kernel, IOptionsMonitor<SemanticKernelOptions> optionsMonitor)
-        {
-            this.kernel = kernel;
-            this.options = optionsMonitor.CurrentValue;
-        }
+    [Route("start")]
+    [HttpPost]
+    public async Task<string[]> StartDemo()
+    {
+        kernel.Config.AddOpenAITextCompletionService(
+            "text-davinci-003",
+            options.Key
+        );
 
-        [Route("start")]
-        [HttpPost]
-        public async Task<string[]> StartDemo()
-        {
-            kernel.Config.AddOpenAITextCompletionService(
-                "text-davinci-003",
-               options.Key
-            );
-
-            var prompt = @"{{$input}}
+        var prompt = @"{{$input}}
 
 One line TLDR with the fewest words.";
 
-            var summarize = kernel.CreateSemanticFunction(prompt);
+        var summarize = kernel.CreateSemanticFunction(prompt);
 
-            string text1 = @"
+        string text1 = @"
 1st Law of Thermodynamics - Energy cannot be created or destroyed.
 2nd Law of Thermodynamics - For a spontaneous process, the entropy of the universe increases.
 3rd Law of Thermodynamics - A perfect crystal at zero Kelvin has zero entropy.";
 
-            string text2 = @"
+        string text2 = @"
 1. An object at rest remains at rest, and an object in motion remains in motion at constant speed and in a straight line unless acted on by an unbalanced force.
 2. The acceleration of an object depends on the mass of the object and the amount of force applied.
 3. Whenever one object exerts a force on another object, the second object exerts an equal and opposite on the first.";
 
-            var text1result = await summarize.InvokeAsync(text1);
+        var text1result = await summarize.InvokeAsync(text1);
 
-            var text2result = await summarize.InvokeAsync(text2);
+        var text2result = await summarize.InvokeAsync(text2);
 
-            string translationPrompt = @"{{$input}}
+        string translationPrompt = @"{{$input}}
 
 Translate the text to math.";
 
-            string summarizePrompt = @"{{$input}}
+        string summarizePrompt = @"{{$input}}
 
 Give me a TLDR with the fewest words.";
 
-            var translator = kernel.CreateSemanticFunction(translationPrompt);
-            summarize = kernel.CreateSemanticFunction(summarizePrompt);
+        var translator = kernel.CreateSemanticFunction(translationPrompt);
+        summarize = kernel.CreateSemanticFunction(summarizePrompt);
 
-            string inputText = @"
+        string inputText = @"
 1st Law of Thermodynamics - Energy cannot be created or destroyed.
 2nd Law of Thermodynamics - For a spontaneous process, the entropy of the universe increases.
 3rd Law of Thermodynamics - A perfect crystal at zero Kelvin has zero entropy.";
 
-            // Run two prompts in sequence (prompt chaining)
-            var text3result = await kernel.RunAsync(inputText, translator, summarize);
+        // Run two prompts in sequence (prompt chaining)
+        var text3result = await kernel.RunAsync(inputText, translator, summarize);
 
-            return new string[] { text1result.Result, text2result.Result, text3result.Result };
-        }
+        return new string[] { text1result.Result, text2result.Result, text3result.Result };
+    }
+
+
+    [Route("init-function")]
+    [HttpPost]
+    public async Task<string> InlineFunction()
+    {
+        kernel.Config.AddOpenAITextCompletionService(
+            "text-davinci-003",
+            options.Key
+        );
+
+        string skPrompt = """
+{{$input}}
+
+Summarize the content above.
+""";
+        var promptConfig = new PromptTemplateConfig
+        {
+            Completion = {
+                MaxTokens = 2000,
+                Temperature = 0.2,
+                TopP = 0.5,
+            }
+        };
+
+        var promptTemplate = new PromptTemplate(
+            skPrompt,                        // Prompt template defined in natural language
+            promptConfig,                    // Prompt configuration
+            kernel                           // SK instance
+        );
+
+        var functionConfig = new SemanticFunctionConfig(promptConfig, promptTemplate);
+
+        var summaryFunction = kernel.RegisterSemanticFunction("MySkill", "Summary", functionConfig);
+
+        var input = """
+Demo (ancient Greek poet)
+From Wikipedia, the free encyclopedia
+Demo or Damo (Greek: Δεμώ, Δαμώ; fl. c. AD 200) was a Greek woman of the Roman period, known for a single epigram, engraved upon the Colossus of Memnon, which bears her name. She speaks of herself therein as a lyric poetess dedicated to the Muses, but nothing is known of her life.[1]
+Identity
+Demo was evidently Greek, as her name, a traditional epithet of Demeter, signifies. The name was relatively common in the Hellenistic world, in Egypt and elsewhere, and she cannot be further identified. The date of her visit to the Colossus of Memnon cannot be established with certainty, but internal evidence on the left leg suggests her poem was inscribed there at some point in or after AD 196.[2]
+Epigram
+There are a number of graffiti inscriptions on the Colossus of Memnon. Following three epigrams by Julia Balbilla, a fourth epigram, in elegiac couplets, entitled and presumably authored by "Demo" or "Damo" (the Greek inscription is difficult to read), is a dedication to the Muses.[2] The poem is traditionally published with the works of Balbilla, though the internal evidence suggests a different author.[1]
+In the poem, Demo explains that Memnon has shown her special respect. In return, Demo offers the gift for poetry, as a gift to the hero. At the end of this epigram, she addresses Memnon, highlighting his divine status by recalling his strength and holiness.[2]
+Demo, like Julia Balbilla, writes in the artificial and poetic Aeolic dialect. The language indicates she was knowledgeable in Homeric poetry—'bearing a pleasant gift', for example, alludes to the use of that phrase throughout the Iliad and Odyssey.[a][2] 
+""";
+
+        var summary = await kernel.RunAsync(input, summaryFunction);
+
+        Console.WriteLine(summary);
+
+        return summary.Result;
     }
 }
