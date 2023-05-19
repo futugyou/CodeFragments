ecb284afeb048285a44cb0014985bfdde9228863
futugyou <17871902+futugyou@users.noreply.github.com>
2023-05-19 18:34:34 +0800

    use SemanticTextMemory
    but has some error

--- AspnetcoreEx/SemanticKernel/SemanticKernelExtensions.cs
+++ AspnetcoreEx/SemanticKernel/SemanticKernelExtensions.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.SemanticKernel.Memory;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel.Connectors.AI.OpenAI.TextEmbedding;
+using Microsoft.SemanticKernel.Memory;
+using Microsoft.SemanticKernel.Services;
 using Microsoft.SemanticKernel.SkillDefinition;
 using Microsoft.SemanticKernel.TemplateEngine;
+using System.Net.Http;
 
 namespace AspnetcoreEx.SemanticKernel;
 public static class SemanticKernelExtensions
@@ -9,8 +13,25 @@ public static class SemanticKernelExtensions
     {
         services.Configure<SemanticKernelOptions>(configuration.GetSection("SemanticKernel"));
         services.AddScoped<KernelConfig>(sp => new KernelConfig());
-        services.AddScoped<ISemanticTextMemory>(sp => NullMemory.Instance);
+        //services.AddScoped<ISemanticTextMemory>(sp => NullMemory.Instance);
         services.AddSingleton<IPromptTemplateEngine, PromptTemplateEngine>();
+        services.AddSingleton<IMemoryStore, VolatileMemoryStore>();
+        services.AddScoped<ISemanticTextMemory>(sp =>
+        {
+            var store = sp.GetRequiredService<IMemoryStore>();
+            var logger = sp.GetRequiredService<ILogger<Program>>();
+            var op = sp.GetRequiredService<IOptionsMonitor<SemanticKernelOptions>>()!.CurrentValue;
+
+            return new SemanticTextMemory(store,
+                new OpenAITextEmbeddingGeneration(
+                    "text-davinci-003",
+                    op.Key,
+                    httpClient: null,
+                    logger: logger
+                    )
+                );
+        });
+
         services.AddScoped<ISkillCollection, SkillCollection>();
         services.AddScoped<IKernel, Kernel>();
 
