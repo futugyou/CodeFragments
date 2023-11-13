71b869caf13a6938c2693f8e576d7df4ee611bfb
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2023-11-13 11:03:57 +0000

    update ssm manger - convert json string to dic

--- AspnetcoreEx/Aws.Extensions.AspNetCore.Configuration/AwsParameterStoreSecretManager.cs
+++ AspnetcoreEx/Aws.Extensions.AspNetCore.Configuration/AwsParameterStoreSecretManager.cs
@@ -1,17 +1,15 @@
+using System.Text.RegularExpressions;
 using Amazon.SimpleSystemsManagement;
 using Amazon.SimpleSystemsManagement.Model;
+using Newtonsoft.Json.Linq;
 
 namespace Aws.Extensions.AspNetCore.Configuration;
 
-public class AwsParameterStoreManager
+public partial class AwsParameterStoreManager
 {
-	public virtual string GetKey(Parameter parameter)
-	{
-		return parameter.Name.Replace("[", ConfigurationPath.KeyDelimiter).Replace("]", ConfigurationPath.KeyDelimiter).Replace("/", ConfigurationPath.KeyDelimiter);
-	}
-
-	// TODO: get all/ get paging/ get by path ...
-	// use  DescribeParametersAsync
+	// TODO: get all/ get paging/ get by path ...  use  DescribeParametersAsync to getall
+	// TODO: parse path like '/AAA/BBB/CCC/DDDD', but data path starts from BBB or CCC
+	// None of the above is important
 	public virtual async Task<IEnumerable<Parameter>> ReadParameters(IAmazonSimpleSystemsManagement awsmanager, AwsClientConfig config)
 	{
 		var sections = config.Section;
@@ -26,26 +24,91 @@ public class AwsParameterStoreManager
 
 	public virtual IDictionary<string, string?> GetData(IEnumerable<Parameter> parameters)
 	{
-		var data = new Dictionary<string, Parameter>(StringComparer.OrdinalIgnoreCase);
+		var dic = new Dictionary<string, string?>(StringComparer.OrdinalIgnoreCase);
 
 		foreach (var parameter in parameters)
 		{
 			string key = GetKey(parameter);
-
-			if (data.TryGetValue(key, out var currentParameter))
+			var added = ToDictionary(key, parameter, dic);
+			if (!added)
 			{
-				if (parameter.LastModifiedDate > currentParameter.LastModifiedDate)
+				if (!dic.ContainsKey(key))
 				{
-					data[key] = parameter;
+					dic.Add(key, parameter.Value);
 				}
 			}
-			else
-			{
-				data.Add(key, parameter);
-			}
-			Console.WriteLine(key + parameter.Value);
 		}
 
-		return data.ToDictionary(d => d.Key, v => v.Value.Value ?? null, StringComparer.OrdinalIgnoreCase);
+		return dic;
 	}
+
+
+	public virtual string GetKey(Parameter parameter)
+	{
+		var segment = parameter.Name.Split('/', StringSplitOptions.RemoveEmptyEntries);
+		return segment[^1];
+	}
+
+	private static bool ToDictionary(string parentKey, Parameter parameter, Dictionary<string, string?> dic)
+	{
+		var result = false;
+		try
+		{
+			var jsonObject = JObject.Parse(parameter.Value);
+			var jTokens = jsonObject.DescendantsAndSelf().Where(p => !p.Any());
+			foreach (var jToken in jTokens)
+			{
+				var key = GetSubKey(parentKey, jToken.Path);
+				if (!dic.ContainsKey(key))
+				{
+					dic.Add(key, jToken.ToString());
+				}
+			}
+
+			result = true;
+		}
+		catch
+		{
+		}
+
+		return result;
+	}
+
+	private static bool ToDictionary(string parentKey, string value, Dictionary<string, string?> dic)
+	{
+		var result = false;
+		try
+		{
+			var jsonObject = JObject.Parse(value);
+			var jTokens = jsonObject.DescendantsAndSelf().Where(p => !p.Any());
+			foreach (var jToken in jTokens)
+			{
+				var key = GetSubKey(parentKey, jToken.Path);
+				if (!dic.ContainsKey(key))
+				{
+					dic.Add(key, jToken.ToString());
+				}
+			}
+
+			result = true;
+		}
+		catch
+		{
+		}
+
+		return result;
+	}
+
+	private static string GetSubKey(string parentKey, string path)
+	{
+		var sub = SubKeyRegex().Replace(path, ConfigurationPath.KeyDelimiter);
+		if (sub.EndsWith(ConfigurationPath.KeyDelimiter))
+		{
+			sub = sub[..^1];
+		}
+		return parentKey + ConfigurationPath.KeyDelimiter + sub;
+	}
+
+	[GeneratedRegex("[^0-9a-zA-Z]+")]
+	private static partial Regex SubKeyRegex();
 }
