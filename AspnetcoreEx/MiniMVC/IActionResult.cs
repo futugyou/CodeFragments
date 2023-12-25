70dd39242824bad6233af300f2e3a22b18cb894c
futugyou <17871902+futugyou@users.noreply.github.com>
2023-12-25 19:24:57 +0800

    action result

---
+++ AspnetcoreEx/MiniMVC/IActionResult.cs
@@ -0,0 +1,18 @@
+using System.Text.Json;
+
+namespace AspnetcoreEx.MiniMVC;
+
+public interface IActionResult
+{
+    Task ExecuteResultAsync(ActionContext  actionContext);
+}
+
+public class JsonResult(object data) : IActionResult
+{
+    public Task ExecuteResultAsync(ActionContext actionContext)
+    {
+        var response = actionContext.HttpContext.Response;
+        response.ContentType = "application/json";
+        return JsonSerializer.SerializeAsync(response.Body, data);
+    }
+}
