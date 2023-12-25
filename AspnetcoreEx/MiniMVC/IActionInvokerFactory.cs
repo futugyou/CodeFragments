f5274c9ea8bf10321d12c4f9bbb5b893f2e05fcc
futugyou <17871902+futugyou@users.noreply.github.com>
2023-12-25 19:36:12 +0800

    implement action invoker

---
+++ AspnetcoreEx/MiniMVC/IActionInvokerFactory.cs
@@ -0,0 +1,12 @@
+
+namespace AspnetcoreEx.MiniMVC;
+
+public interface IActionInvokerFactory
+{
+    IActionInvoker CreateInvoker(ActionContext actionContext);
+}
+
+public class ActionInvokerFactory : IActionInvokerFactory
+{
+    public IActionInvoker CreateInvoker(ActionContext actionContext) => new ActionInvoker(actionContext);
+}
