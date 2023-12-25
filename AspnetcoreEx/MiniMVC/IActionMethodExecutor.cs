de53381d93b797973f925f8443a0205a85f460dd
futugyou <17871902+futugyou@users.noreply.github.com>
2023-12-25 19:21:53 +0800

    action method executor

---
+++ AspnetcoreEx/MiniMVC/IActionMethodExecutor.cs
@@ -0,0 +1,33 @@
+using System.Collections.Concurrent;
+using System.Reflection;
+using System.Linq.Expressions;
+
+namespace AspnetcoreEx.MiniMVC;
+
+public interface IActionMethodExecutor
+{
+    object? Execute(object controller, ActionDescriptor actionDescriptor, object?[] arguments);
+}
+
+public class ActionMethodExecutor : IActionMethodExecutor
+{
+    private readonly ConcurrentDictionary<MethodInfo, Func<object, object?[], object?>> _executors = new();
+    public object? Execute(object controller, ActionDescriptor actionDescriptor, object?[] arguments)
+        => _executors.GetOrAdd(actionDescriptor.MethodInfo, CreateExecutor).Invoke(controller, arguments);
+    private Func<object, object?[], object?> CreateExecutor(MethodInfo methodInfo)
+    {
+        var controller = Expression.Parameter(typeof(object));
+        var arguments = Expression.Parameter(typeof(object?[]));
+
+        var parameters = methodInfo.GetParameters();
+        var convertedArguments = new Expression[parameters.Length];
+        for (int index = 0; index < parameters.Length; index++)
+        {
+            convertedArguments[index] = Expression.Convert(Expression.ArrayIndex(arguments, Expression.Constant(index)), parameters[index].ParameterType);
+        }
+
+        var convertedController = Expression.Convert(controller, methodInfo.DeclaringType!);
+        var call = Expression.Call(convertedController, methodInfo, convertedArguments);
+        return Expression.Lambda<Func<object, object?[], object?>>(call, controller, arguments).Compile();
+    }
+}
