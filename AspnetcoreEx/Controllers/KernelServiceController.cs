617af37cd10ca3bc0ebaca489daa8abef03054f9
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-01-17 10:29:08 +0000

    use handlebars package

--- AspnetcoreEx/Controllers/KernelServiceController.cs
+++ AspnetcoreEx/Controllers/KernelServiceController.cs
@@ -2,6 +2,7 @@
 using AspnetcoreEx.KernelService;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.PromptTemplate.Handlebars;
 
 namespace AspnetcoreEx.Controllers;
 
@@ -52,25 +53,28 @@ public class KernelServiceController : ControllerBase
         var responseList = new List<string>();
         string request = "I want to send an email to the marketing team celebrating their recent milestone.";
         responseList.Add(request);
-        string history = @"<message role=""user"">I hate sending emails, no one ever reads them.</message>
-<message role=""assistant"">I'm sorry to hear that. Messages may be a better way to communicate.</message>";
+        string history = @"
+        <message role=""user"">I hate sending emails, no one ever reads them.</message>
+        <message role=""assistant"">I'm sorry to hear that. Messages may be a better way to communicate.</message>";
 
-        string prompt = @$"<message role=""system"">Instructions: What is the intent of this request?
-If you don't know the intent, don't guess; instead respond with ""Unknown"".
-Choices: SendEmail, SendMessage, CompleteTask, CreateDocument, Unknown.
-Bonus: You'll get $20 if you get this right.</message>
+        string prompt = @$"
+        <message role=""system"">Instructions: What is the intent of this request?
+        If you don't know the intent, don't guess; instead respond with ""Unknown"".
+        Choices: SendEmail, SendMessage, CompleteTask, CreateDocument, Unknown.
+        Bonus: You'll get $20 if you get this right.</message>
 
-<message role=""user"">Can you send a very quick approval to the marketing team?</message>
-<message role=""system"">Intent:</message>
-<message role=""assistant"">SendMessage</message>
+        <message role=""user"">Can you send a very quick approval to the marketing team?</message>
+        <message role=""system"">Intent:</message>
+        <message role=""assistant"">SendMessage</message>
 
-<message role=""user"">Can you send the full update to the marketing team?</message>
-<message role=""system"">Intent:</message>
-<message role=""assistant"">SendEmail</message>
+        <message role=""user"">Can you send the full update to the marketing team?</message>
+        <message role=""system"">Intent:</message>
+        <message role=""assistant"">SendEmail</message>
 
-{history}
-<message role=""user"">{request}</message>
-<message role=""system"">Intent:</message>";
+        {history}
+        <message role=""user"">{request}</message>
+        <message role=""system"">Intent:</message>
+        ";
 
         var result = await _kernel.InvokePromptAsync(prompt);
         responseList.Add(result.ToString());
@@ -86,17 +90,19 @@ Bonus: You'll get $20 if you get this right.</message>
         string request = "I want to send an email to the marketing team celebrating their recent milestone.";
         responseList.Add(request);
         var chat = _kernel.CreateFunctionFromPrompt(
-    @"{{$history}}
-    User: {{$request}}
-    Assistant: "
-);
+            @"{{$history}}
+            User: {{$request}}
+            Assistant: "
+        );
+
         var chatResult = _kernel.InvokeStreamingAsync<StreamingChatMessageContent>(
-        chat,
-        new() {
-            { "request", request },
-            { "history", string.Join("\n", history.Select(x => x.Role + ": " + x.Content)) }
-        }
-    );
+            chat,
+            new() {
+                { "request", request },
+                { "history", string.Join("\n", history.Select(x => x.Role + ": " + x.Content)) }
+            }
+        );
+
         // Stream the response
         string message = "";
         await foreach (var chunk in chatResult)
@@ -111,6 +117,70 @@ Bonus: You'll get $20 if you get this right.</message>
         history.AddUserMessage(request!);
         history.AddAssistantMessage(message);
 
+        return [.. responseList];
+    }
+    
+    [Route("prompt/handlebars")]
+    [HttpPost]
+    public async Task<string[]> PromptHandlebars()
+    {
+        var responseList = new List<string>();
+        string request = "I want to send an email to the marketing team celebrating their recent milestone.";
+        ChatHistory history = [];
+        var getIntent = _kernel.CreateFunctionFromPrompt(
+            new()
+            {
+                Template = @"
+                <message role=""system"">Instructions: What is the intent of this request?
+                Do not explain the reasoning, just reply back with the intent. If you are unsure, reply with {{choices[0]}}.
+                Choices: {{choices}}.</message>
+
+                {{#each fewShotExamples}}
+                    {{#each this}}
+                        <message role=""{{role}}"">{{content}}</message>
+                    {{/each}}
+                {{/each}}
+
+                {{#each chatHistory}}
+                    <message role=""{{role}}"">{{content}}</message>
+                {{/each}}
+
+                <message role=""user"">{{request}}</message>
+                <message role=""system"">Intent:</message>",
+                TemplateFormat = "handlebars"
+            },
+            new HandlebarsPromptTemplateFactory()
+        );
+
+        // Create choices
+        List<string> choices = ["ContinueConversation", "EndConversation"];
+
+        // Create few-shot examples
+        List<ChatHistory> fewShotExamples = [
+            [
+                new ChatMessageContent(AuthorRole.User, "Can you send a very quick approval to the marketing team?"),
+                new ChatMessageContent(AuthorRole.System, "Intent:"),
+                new ChatMessageContent(AuthorRole.Assistant, "ContinueConversation"),
+            ],
+            [
+                new ChatMessageContent(AuthorRole.User, "Thanks, I'm done for now"),
+                new ChatMessageContent(AuthorRole.System, "Intent:"),
+                new ChatMessageContent(AuthorRole.Assistant, "EndConversation")
+            ]
+        ];
+
+        // Invoke prompt
+        var intent = await _kernel.InvokeAsync(
+            getIntent,
+            new() {
+                { "request", request },
+                { "choices", choices },
+                { "history", history },
+                { "fewShotExamples", fewShotExamples }
+            }
+        );
+
+        responseList.Add(intent.ToString());
         return [.. responseList];
     }
 
