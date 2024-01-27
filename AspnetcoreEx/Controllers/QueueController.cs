b794d7db6bd9d0018c77eae954f469e5cf7cf3a1
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-01-27 09:05:08 +0000

    fix queue error

--- AspnetcoreEx/Controllers/QueueController.cs
+++ AspnetcoreEx/Controllers/QueueController.cs
@@ -20,7 +20,7 @@ public class QueueController : ControllerBase
     [HttpGet]
     public async Task Get()
     {
-        await taskQueue.QueueBackgroundWorkItemAsync(BuildWorkItemAsync)
+        await taskQueue.QueueBackgroundWorkItemAsync(BuildWorkItemAsync);
     }
 
     private async ValueTask BuildWorkItemAsync(CancellationToken token)
@@ -28,34 +28,20 @@ public class QueueController : ControllerBase
         // Simulate three 5-second tasks to complete
         // for each enqueued work item
 
-        int delayLoop = 0;
         var guid = Guid.NewGuid();
 
         logger.LogInformation("Queued work item {Guid} is starting.", guid);
 
-        while (!token.IsCancellationRequested && delayLoop < 3)
+        try
         {
-            try
-            {
-                await Task.Delay(TimeSpan.FromSeconds(5), token);
-            }
-            catch (OperationCanceledException)
-            {
-                // Prevent throwing if the Delay is cancelled
-            }
-
-            ++delayLoop;
-
-            logger.LogInformation("Queued work item {Guid} is running. {DelayLoop}/3", guid, delayLoop);
+            await Task.Delay(TimeSpan.FromSeconds(5), token);
+        }
+        catch (OperationCanceledException)
+        {
+            // Prevent throwing if the Delay is cancelled
         }
 
-        if (delayLoop is 3)
-        {
-            logger.LogInformation("Queued Background Task {Guid} is complete.", guid);
-        }
-        else
-        {
-            logger.LogInformation("Queued Background Task {Guid} was cancelled.", guid);
-        }
+        logger.LogInformation("Queued Background Task {Guid} is complete.", guid);
+
     }
 }
