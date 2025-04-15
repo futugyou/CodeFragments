70747e10daff2ab3369e1cc74fb7cd14c856e41c
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2025-04-15 04:46:44 +0000

    Updated mqtt fixed DI

--- AspnetcoreEx/MQTT/MQTTExtensions.cs
+++ AspnetcoreEx/MQTT/MQTTExtensions.cs
@@ -11,20 +11,6 @@ public static class MQTTExtensions
         services.Configure<MQTTOptions>(configuration.GetSection("MQTT"));
         services.AddHostedService<MqttBackgroundSubscriber>();
         services.AddSingleton<IMqttSubscribeHandler, MqttSubscribeHandler>();
-        // This is for publishing messages
-        services.AddSingleton(async sp =>
-        {
-            var op = sp.GetService<IOptionsMonitor<MQTTOptions>>()!.CurrentValue;
-            var factory = new MqttClientFactory();
-            var client = factory.CreateMqttClient();
-            var clientOptions = new MqttClientOptionsBuilder()
-            .WithTcpServer(op.Host, op.Port)
-            .WithCredentials(op.UserName, op.Password)
-            .Build();
-
-            await client.ConnectAsync(clientOptions, default);
-            return factory.CreateMqttClient();
-        });
         services.AddSingleton<IMqttService, MqttService>();
 
         return services;
--- AspnetcoreEx/MQTT/MqttService.cs
+++ AspnetcoreEx/MQTT/MqttService.cs
@@ -4,21 +4,30 @@ namespace AspnetcoreEx.MQTT;
 
 public class MqttService : IMqttService
 {
-    private readonly IMqttClient _mqttClient;
+    private readonly IOptionsMonitor<MQTTOptions> _optionsMonitor;
 
-    public MqttService(IMqttClient mqttClient)
+    public MqttService(IOptionsMonitor<MQTTOptions> optionsMonitor)
     {
-        _mqttClient = mqttClient;
+        _optionsMonitor = optionsMonitor;
     }
 
     public async Task PublishAsync(string topic, string payload)
     {
+        var factory = new MqttClientFactory();
+        var client = factory.CreateMqttClient();
+        var op = _optionsMonitor.CurrentValue;
+        var clientOptions = new MqttClientOptionsBuilder()
+                .WithTcpServer(op.Host, op.Port)
+                .WithCredentials(op.UserName, op.Password)
+                .Build();
+        await client.ConnectAsync(clientOptions, default);
+
         var message = new MqttApplicationMessageBuilder()
             .WithTopic(topic)
             .WithPayload(payload)
             .WithQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce)
             .Build();
 
-        await _mqttClient.PublishAsync(message);
+        await client.PublishAsync(message);
     }
 }
