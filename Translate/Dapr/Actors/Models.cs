da0ec378d348072e4d7e647e79fb14fed17976ea
futugyou <17871902+futugyou@users.noreply.github.com>
2025-05-02 15:11:12 +0000

    Add SaveStateRequest, StateResponse, BulkStateResponse, and TimerResponse classes with JSON serialization support

--- Translate/Dapr/Actors/Models.cs
+++ Translate/Dapr/Actors/Models.cs
@@ -7,7 +7,7 @@ namespace Actors;
 
 public class Const
 {
-   public const string DaprSeparator = "||";
+    public const string DaprSeparator = "||";
 }
 
 
@@ -70,7 +70,7 @@ public class ReminderResponse
     public sealed class JsonConverter : JsonConverter<ReminderResponse>
     {
         public override ReminderResponse? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        { 
+        {
             throw new NotImplementedException("Deserialization not implemented.");
         }
 
@@ -94,7 +94,7 @@ public class ReminderResponse
                     writer.WriteBase64StringValue(bytesValue.Value.ToByteArray());
                 }
                 else
-                { 
+                {
                     var unpacked = TryUnpackKnownType(value.Data);
                     if (unpacked != null)
                     {
@@ -104,7 +104,104 @@ public class ReminderResponse
                     }
                     else
                     {
-                        writer.WriteNullValue();  
+                        writer.WriteNullValue();
+                    }
+                }
+            }
+
+            writer.WriteEndObject();
+        }
+
+        private IMessage TryUnpackKnownType(WellKnownTypes.Any any)
+        {
+            // TODO:  
+            // if (any.TypeUrl.EndsWith("sametype"))
+            //     return any.Unpack<Object>();
+
+            return null!;
+        }
+    }
+}
+
+public class SaveStateRequest
+{
+    [JsonPropertyName("actorId")]
+    public string ActorID { get; set; }
+    [JsonPropertyName("actorType")]
+    public string ActorType { get; set; }
+    [JsonPropertyName("keys")]
+    public string[] Keys { get; set; }
+    [JsonPropertyName("value")]
+    public WellKnownTypes.Any Value { get; set; }
+}
+
+public class StateResponse
+{
+    [JsonPropertyName("data")]
+    public byte[] Data { get; set; }
+    [JsonPropertyName("metadata")]
+    public Dictionary<string, string> Metadata { get; set; }
+}
+
+public class BulkStateResponse
+{
+    [JsonPropertyName("data")]
+    public Dictionary<string, byte[]> Data { get; set; }
+}
+
+[JsonConverter(typeof(TimerResponse.JsonConverter))]
+public class TimerResponse
+{
+    [JsonPropertyName("data")]
+    public WellKnownTypes.Any Data { get; set; }
+    [JsonPropertyName("callback")]
+    public string Callback { get; set; }
+    [JsonPropertyName("dueTime")]
+    public string DueTime { get; set; }
+    [JsonPropertyName("period")]
+    public string Period { get; set; }
+
+    public sealed class JsonConverter : JsonConverter<TimerResponse>
+    {
+        public override TimerResponse? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException("Deserialization not implemented.");
+        }
+
+        public override void Write(Utf8JsonWriter writer, TimerResponse value, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+
+            if (!string.IsNullOrEmpty(value.DueTime))
+                writer.WriteString("dueTime", value.DueTime);
+
+            if (!string.IsNullOrEmpty(value.Period))
+                writer.WriteString("period", value.Period);
+
+            if (!string.IsNullOrEmpty(value.Callback))
+                writer.WriteString("callback", value.Callback);
+
+            if (value.Data != null)
+            {
+                writer.WritePropertyName("data");
+
+                if (value.Data.TypeUrl.EndsWith("BytesValue"))
+                {
+                    var bytesValue = value.Data.Unpack<WellKnownTypes.BytesValue>();
+                    writer.WriteBase64StringValue(bytesValue.Value.ToByteArray());
+                }
+                else
+                {
+                    var unpacked = TryUnpackKnownType(value.Data);
+                    if (unpacked != null)
+                    {
+                        var json = JsonFormatter.Default.Format(unpacked);
+                        using var doc = JsonDocument.Parse(json);
+                        doc.RootElement.WriteTo(writer);
+                    }
+                    else
+                    {
+                        writer.WriteNullValue();
                     }
                 }
             }
