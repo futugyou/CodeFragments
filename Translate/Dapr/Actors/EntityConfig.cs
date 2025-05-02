97b5b32efcfcce4c0c6bf12822e2c2892a52f8ee
futugyou <17871902+futugyou@users.noreply.github.com>
2025-05-02 15:42:38 +0000

    Added ProjectReference to Config project and implement EntityConfig class for actor system configuration

---
+++ Translate/Dapr/Actors/EntityConfig.cs
@@ -0,0 +1,33 @@
+using Config;
+
+namespace Actors;
+
+public class EntityConfig
+{
+    public string[] Entities { get; set; }
+    public TimeSpan ActorIdleTimeout { get; set; }
+    public TimeSpan DrainOngoingCallTimeout { get; set; }
+    public bool DrainRebalancedActors { get; set; }
+    public ReentrancyConfig ReentrancyConfig { get; set; }
+    public int RemindersStoragePartitions { get; set; } = 1;
+
+    public EntityConfig(Config.EntityConfig config)
+    {
+        Entities = config.Entities;
+        ActorIdleTimeout = Const.DefaultIdleTimeout;
+        DrainOngoingCallTimeout = Const.DefaultOngoingCallTimeout;
+        DrainRebalancedActors = config.DrainRebalancedActors;
+        ReentrancyConfig = config.Reentrancy;
+        RemindersStoragePartitions = config.RemindersStoragePartitions;
+
+        if (config.ActorIdleTimeout != null)
+        {
+            ActorIdleTimeout = TimeSpan.Parse(config.ActorIdleTimeout);
+        }
+
+        if (config.Reentrancy.MaxStackDepth != 0)
+        {
+            ReentrancyConfig.MaxStackDepth = config.Reentrancy.MaxStackDepth;
+        }
+    }
+}
