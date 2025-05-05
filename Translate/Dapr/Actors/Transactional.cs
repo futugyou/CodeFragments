6862e90ad9bbc8c9cd63f0edc0caf3bc59d4af0e
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2025-05-05 07:09:23 +0000

    Added a project reference to Dapr.Abstractions implement the state operation methods of TransactionalOperation and TransactionalUpsert

--- Translate/Dapr/Actors/Transactional.cs
+++ Translate/Dapr/Actors/Transactional.cs
@@ -1,3 +1,5 @@
+using Dapr.Abstractions;
+
 namespace Actors;
 
 public class StateOperationOpts
@@ -7,11 +9,6 @@ public class StateOperationOpts
     public bool StateTTLEnabled { get; set; }
 }
 
-public enum OperationType
-{
-    Upsert,
-    Delete
-}
 
 public class TransactionalRequest
 {
@@ -31,8 +28,36 @@ public class TransactionalOperation
     public OperationType Operation { get; set; }
     [JsonPropertyName("request")]
     public object Request { get; set; }
-}
 
+    public ITransactionalStateOperation StateOperation(string baseKey, StateOperationOpts opts)
+    {
+        ArgumentNullException.ThrowIfNull(Request, nameof(Request));
+
+        switch (Request)
+        {
+            case TransactionalUpsert upsert when Operation == OperationType.Upsert:
+                return upsert.StateOperation(baseKey, opts);
+            case TransactionalDelete delete when Operation == OperationType.Delete:
+                return delete.StateOperation(baseKey, opts);
+            default:
+                // Fallback to use AutoMapper or System.TexJson for dynamic mapping
+                if (Operation == OperationType.Upsert)
+                {
+                    var upsert = JsonSerializer.Deserialize<TransactionalUpsert>(JsonSerializer.Serialize(Request)) ?? throw new InvalidOperationException("upsert request is null");
+                    return upsert.StateOperation(baseKey, opts);
+                }
+                else if (Operation == OperationType.Delete)
+                {
+                    var delete = JsonSerializer.Deserialize<TransactionalDelete>(JsonSerializer.Serialize(Request)) ?? throw new InvalidOperationException("delete request is null");
+                    return delete.StateOperation(baseKey, opts);
+                }
+                else
+                {
+                    throw new InvalidOperationException($"operation type {Operation} not supported");
+                }
+        }
+    }
+}
 
 public class TransactionalUpsert
 {
@@ -44,6 +69,38 @@ public class TransactionalUpsert
     public string ETag { get; set; }
     [JsonPropertyName("metadata")]
     public Dictionary<string, string> Metadata { get; set; }
+
+    public ITransactionalStateOperation StateOperation(string baseKey, StateOperationOpts opts)
+    {
+        ArgumentNullException.ThrowIfNull(Key, nameof(Key));
+
+        if (Metadata == null || Metadata.Count == 0)
+        {
+            Metadata = opts.Metadata;
+
+        }
+        else
+        {
+            foreach (var d in opts.Metadata)
+            {
+                Metadata[d.Key] = d.Value;
+            }
+        }
+
+        if (!opts.StateTTLEnabled && Metadata.ContainsKey("ttlInSeconds"))
+        {
+            throw new InvalidOperationException("ttlInSeconds is not supported without the 'ActorStateTTL' feature enabled");
+        }
+
+        return new SetRequest
+        {
+            Key = baseKey + Key,
+            Value = Value,
+            Metadata = Metadata,
+            ETag = ETag,
+            ContentType = opts.ContentType,
+        };
+    }
 }
 
 
@@ -53,4 +110,15 @@ public class TransactionalDelete
     public string Key { get; set; }
     [JsonPropertyName("etag")]
     public string ETag { get; set; }
+
+    public ITransactionalStateOperation StateOperation(string baseKey, StateOperationOpts opts)
+    {
+        ArgumentNullException.ThrowIfNull(Key, nameof(Key));
+        return new DeleteRequest
+        {
+            Key = baseKey + Key,
+            Metadata = opts.Metadata,
+            ETag = ETag,
+        };
+    }
 }
