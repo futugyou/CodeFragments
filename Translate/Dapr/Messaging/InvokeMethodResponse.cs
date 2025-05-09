0cb6040817eb994caf58eead18a15a670f3ce41a
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2025-05-09 16:49:27 +0000

    Added InvokeMethodResponse

---
+++ Translate/Dapr/Messaging/InvokeMethodResponse.cs
@@ -0,0 +1,65 @@
+﻿using Dapr.Client.Autogen.Grpc.v1;
+using Dapr.Proto.Internals.V1;
+using Google.Protobuf.WellKnownTypes;
+
+namespace Messaging;
+
+public class InvokeMethodResponse
+{
+    private InternalInvokeResponse r;
+    public string DataTypeURL
+    {
+        get => dataTypeURL;
+        set => dataTypeURL = value;
+    }
+
+    private string dataTypeURL;
+
+    public InvokeMethodResponse(int statusCode, string statusMessage, Any[] statusDetails)
+    {
+        var status = new Status { Code = statusCode, Message = statusMessage };
+        status.Details.AddRange(statusDetails);
+        r = new InternalInvokeResponse()
+        {
+            Status = status,
+            Message = new InvokeResponse { },
+        };
+    }
+
+    public InvokeMethodResponse(InternalInvokeResponse request)
+    {
+        r = request;
+    }
+
+    public InvokeMethodResponse WithMessage(InvokeResponse pb)
+    {
+        r.Message = pb;
+        return this;
+    }
+
+    public InvokeMethodResponse WithContentType(string contentType)
+    {
+        r.Message.ContentType = contentType;
+        return this;
+    }
+
+    public bool IsHTTPResponse()
+    {
+        if (r == null)
+        {
+            return false;
+        }
+
+        return r.Status.Code >= 100;
+    }
+
+    public Dictionary<string, ListStringValue>? Trailers()
+    {
+        if (r == null || r.Trailers == null)
+        {
+            return null;
+        }
+
+        return r.Trailers.ToDictionary(x => x.Key, x => x.Value);
+    }
+}
