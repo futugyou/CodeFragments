95eae5d3dd19c0470839708014a24d0f5f9673ff
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2025-07-12 18:07:33 +0000

    Added custom `AddGoogleAIGeminiChatCompletion` because GoogleAIGeminiChatCompletionService did not implement ITextGenerationService

--- AspnetcoreEx/KernelService/KernelBuilderExtensions.cs
+++ AspnetcoreEx/KernelService/KernelBuilderExtensions.cs
@@ -3,13 +3,46 @@
 using System.ClientModel;
 using System.ClientModel.Primitives;
 using Microsoft.Extensions.AI;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.Google;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.TextGeneration;
 using OpenAI;
 
 namespace AspnetcoreEx.KernelService;
 
+[Experimental("SKEXP0010")]
 public static class KernelBuilderExtensions
 {
+    public static IKernelBuilder AddGoogleAIGeminiChatCompletion(
+        this IKernelBuilder builder,
+        string modelId,
+        string apiKey,
+        Uri endpoint,
+        GoogleAIVersion apiVersion = GoogleAIVersion.V1_Beta, // todo: change beta to stable when stable version will be available
+        string? serviceId = null,
+        HttpClient? httpClient = null)
+    {
+
+        OpenAIChatCompletionService Factory(IServiceProvider serviceProvider, object? _) =>
+                    new(modelId: modelId,
+                        apiKey: apiKey,
+                        endpoint: endpoint,
+                        httpClient: HttpClientProvider.GetHttpClient(httpClient, serviceProvider),
+                        loggerFactory: serviceProvider.GetService<ILoggerFactory>());
+
+        builder.Services.AddKeyedSingleton<IChatCompletionService>(serviceId, (serviceProvider, _) =>
+            new GoogleAIGeminiChatCompletionService(
+                modelId: modelId,
+                apiKey: apiKey,
+                apiVersion: apiVersion,
+                httpClient: HttpClientProvider.GetHttpClient(httpClient, serviceProvider),
+                loggerFactory: serviceProvider.GetService<ILoggerFactory>()));
+
+        builder.Services.AddKeyedSingleton<ITextGenerationService>(serviceId, (Func<IServiceProvider, object?, OpenAIChatCompletionService>)Factory);
+        return builder;
+    }
+
     public static IKernelBuilder AddOpenAIEmbeddingGenerator(
              this IKernelBuilder builder,
              string modelId,
@@ -50,7 +83,7 @@ public static class KernelBuilderExtensions
 
             var builder = new OpenAIClient(
                    credential: new ApiKeyCredential(apiKey),
-                   options: GetOpenAIClientOptions(httpClient: httpClient, endpoint: endpoint, orgId: orgId))
+                   options: GetOpenAIClientOptions(httpClient: HttpClientProvider.GetHttpClient(httpClient, serviceProvider), endpoint: endpoint, orgId: orgId))
                .GetEmbeddingClient(modelId)
                .AsIEmbeddingGenerator(dimensions)
                .AsBuilder()
