87284196e55939ca484c2ff17afd44830f712963
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2025-07-17 19:08:53 +0000

    Added tool to parse HTML

---
+++ AspnetcoreEx/KernelService/Duckduckgo/DuckTextSearchOptions.cs
@@ -0,0 +1,10 @@
+
+
+namespace AspnetcoreEx.KernelService.Duckduckgo;
+
+public class DuckTextSearchOptions
+{
+    public Uri? Endpoint { get; init; } = null;
+    public HttpClient? HttpClient { get; init; } = null;
+    public ILoggerFactory? LoggerFactory { get; init; } = null;
+}
