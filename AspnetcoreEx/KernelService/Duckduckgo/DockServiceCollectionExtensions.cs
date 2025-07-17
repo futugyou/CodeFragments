6473eb82ef467007714c846abec4ed452e23b558
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2025-07-17 19:09:49 +0000

    Added duck search to DI

---
+++ AspnetcoreEx/KernelService/Duckduckgo/DockServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+
+using Microsoft.SemanticKernel.Data;
+
+namespace AspnetcoreEx.KernelService.Duckduckgo;
+
+public static class DockServiceCollectionExtensions
+{
+    public static IServiceCollection AddDuckduckgoTextSearch(
+        this IServiceCollection services,
+        DuckTextSearchOptions? options = null,
+        string? serviceId = default)
+    {
+
+        services.AddKeyedSingleton<ITextSearch>(
+            serviceId,
+            (sp, obj) =>
+            {
+                var selectedOptions = options ?? sp.GetService<DuckTextSearchOptions>();
+
+                return new DuckduckgoTextSearch(selectedOptions);
+            });
+
+        return services;
+    }
+}
