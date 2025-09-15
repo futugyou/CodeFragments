b45106a56ff7cda8066231dfdbf704255a3c948a
futugyou <17871902+futugyou@users.noreply.github.com>
2025-09-15 15:54:41 +0000

    Added declarative prompt template usage

--- AspnetcoreEx/Controllers/SKDeclarativeController.cs
+++ AspnetcoreEx/Controllers/SKDeclarativeController.cs
@@ -72,4 +72,48 @@ public class SKDeclarativeController : ControllerBase
             yield return $"Role: {response.Role}, Content: {response.Content}";
         }
     }
+
+    [Route("template")]
+    [HttpPost]
+    public async IAsyncEnumerable<string> Template(string topic = "Dogs", string length = "3")
+    {
+        var text =
+            """
+            type: chat_completion_agent
+            name: StoryAgent
+            description: A agent that generates a story about a topic.
+            instructions: Tell a story about {{$topic}} that is {{$length}} sentences long.
+            inputs:
+                topic:
+                    description: The topic of the story.
+                    required: true
+                    default: Cats
+                length:
+                    description: The number of sentences in the story.
+                    required: true
+                    default: 2
+            outputs:
+                output1:
+                    description: output1 description
+            template:
+                format: semantic-kernel
+            """;
+        var agentFactory = new ChatCompletionAgentFactory();
+        var promptTemplateFactory = new KernelPromptTemplateFactory();
+
+        var agent = await agentFactory.CreateAgentFromYamlAsync(text, new() { Kernel = _kernel, PromptTemplateFactory = promptTemplateFactory });
+        var options = new AgentInvokeOptions()
+        {
+            KernelArguments = new()
+            {
+                { "topic", topic },
+                { "length", length },
+            }
+        };
+
+        await foreach (ChatMessageContent response in agent!.InvokeAsync(Array.Empty<ChatMessageContent>(), options: options))
+        {
+            yield return $"Role: {response.Role}, Content: {response.Content}";
+        }
+    }
 }
