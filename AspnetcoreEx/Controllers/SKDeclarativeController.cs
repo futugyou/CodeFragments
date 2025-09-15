5095bc3eead7483e56427053d46e20cbf5f9e435
futugyou <17871902+futugyou@users.noreply.github.com>
2025-09-15 15:49:04 +0000

    Added function to  usage

--- AspnetcoreEx/Controllers/SKDeclarativeController.cs
+++ AspnetcoreEx/Controllers/SKDeclarativeController.cs
@@ -1,6 +1,7 @@
 ﻿
 using AspnetcoreEx.KernelService;
 using Microsoft.SemanticKernel;
+using AspnetcoreEx.KernelService.Skills;
 using Microsoft.SemanticKernel.Agents;
 using Microsoft.SemanticKernel.ChatCompletion;
 
@@ -22,10 +23,10 @@ public class SKDeclarativeController : ControllerBase
 
     [Route("chat")]
     [HttpPost]
-    public async IAsyncEnumerable<string> Chat(string query="Cats and Dogs")
+    public async IAsyncEnumerable<string> Chat(string query = "Cats and Dogs")
     {
-       var text =
-            """
+        var text =
+             """
             type: chat_completion_agent
             name: StoryAgent
             description: Story Telling Agent
@@ -37,7 +38,38 @@ public class SKDeclarativeController : ControllerBase
 
         await foreach (ChatMessageContent response in agent!.InvokeAsync(query))
         {
-             yield return $"Role: {response.Role}, Content: {response.Content}";
+            yield return $"Role: {response.Role}, Content: {response.Content}";
+        }
+    }
+
+    [Route("func")]
+    [HttpPost]
+    public async IAsyncEnumerable<string> Function(string query = "Can you tell me the status of all the lights?")
+    {
+        _kernel.Plugins.Clear();
+        _kernel.Plugins.AddFromType<LightPlugin>("Lights");
+        var text =
+            """
+            type: chat_completion_agent
+            name: FunctionCallingAgent
+            instructions: Use the provided functions to answer questions about the lights.
+            description: This agent uses the provided functions to answer questions about the lights.
+            model:
+              options:
+                temperature: 0.4
+            tools:
+              - id: Lights.get_lights
+                type: function
+              - id: Lights.change_state
+                type: function
+            """;
+        var agentFactory = new ChatCompletionAgentFactory();
+
+        var agent = await agentFactory.CreateAgentFromYamlAsync(text, new() { Kernel = _kernel });
+
+        await foreach (ChatMessageContent response in agent!.InvokeAsync(new ChatMessageContent(AuthorRole.User, query)))
+        {
+            yield return $"Role: {response.Role}, Content: {response.Content}";
         }
     }
 }
