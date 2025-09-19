39653417a608a0c20268fe99a1528378db646b7f
futugyou <17871902+futugyou@users.noreply.github.com>
2025-09-19 14:52:33 +0000

    Added GraphQLOptions

---
+++ AspnetcoreEx/GraphQL/GraphQLOptions.cs
@@ -0,0 +1,9 @@
+
+namespace AspnetcoreEx.GraphQL;
+
+public class GraphQLOptions
+{
+    public string SecurityKey { get; set; }
+    public string ValidIssuer { get; set; }
+    public string ValidAudience { get; set; }
+}
