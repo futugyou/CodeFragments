fcb9597461220ea5fc642c982116b476cbdc4dd5
futugyou <17871902+futugyou@users.noreply.github.com>
2026-01-03 22:42:41 +0000

    Added an empty, independent AgentStack project.

--- KaleidoCode/src/KaleidoCode.AppHost/AppHost.cs
+++ KaleidoCode/src/KaleidoCode.AppHost/AppHost.cs
@@ -1,5 +1,9 @@
+using Aspire.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var api = builder.AddProject<Projects.Webhost>("api");
-    
+
+var maf = builder.AddProject<Projects.AgentStack>("maf");
+
 builder.Build().Run();
