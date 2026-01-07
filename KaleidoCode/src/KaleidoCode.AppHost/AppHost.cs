f02891e646435ca5eca9ff18761225908fa0ac0f
futugyou <17871902+futugyou@users.noreply.github.com>
2026-01-07 20:08:08 +0000

    Refactored KaleidoCode project structure

--- KaleidoCode/src/KaleidoCode.AppHost/AppHost.cs
+++ KaleidoCode/src/KaleidoCode.AppHost/AppHost.cs
@@ -2,12 +2,16 @@ using Aspire.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var api = builder.AddProject<Projects.Webhost>("api");
+var api = builder.AddProject<Projects.KaleidoCode>("api");
 
 var maf = builder.AddProject<Projects.AgentStack>("maf");
 
 var graphql = builder.AddProject<Projects.GraphQLStack>("graphql");
 
 var es = builder.AddProject<Projects.OpenSearchStack>("es");
+
+var sk = builder.AddProject<Projects.SemanticKernelStack>("sk");
+
+var km = builder.AddProject<Projects.KernelMemoryStack>("km");
 
 builder.Build().Run();
