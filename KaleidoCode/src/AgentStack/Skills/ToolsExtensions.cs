9818c74e9f8f7d8bae8fd48b0787e0cac517257c
futugyou <17871902+futugyou@users.noreply.github.com>
2026-01-19 16:41:02 +0000

    Updated tool extensions, hidden inherited methods

--- KaleidoCode/src/AgentStack/Skills/ToolsExtensions.cs
+++ KaleidoCode/src/AgentStack/Skills/ToolsExtensions.cs
@@ -7,29 +7,27 @@ using System.Text.Json.Serialization;
 public class ToolsExtensions
 {
 
-    public static AITool[] GetAIToolsFromType<T>() where T : new()
+    public static AITool[] GetAIToolsFromType<T>() where T : class, new()
     {
         var obj = new T();
-        return [
-            .. typeof(T)
-                .GetMethods(BindingFlags.Static | BindingFlags.Public)
-                .Select((m) => AIFunctionFactory.Create(m, target: null)), // Get from type static methods
-            .. obj.GetType()
-                .GetMethods(BindingFlags.Instance | BindingFlags.Public)
-                .Select((m) => AIFunctionFactory.Create(m, target: obj)) // Get from instance methods
-        ];
+        return Creator(obj);
     }
 
-    public static AITool[] GetAIToolsFromType<T>(IServiceProvider serviceProvider) where T : notnull
+    public static AITool[] GetAIToolsFromType<T>(IServiceProvider serviceProvider) where T : class
     {
         T obj = serviceProvider.GetRequiredService<T>();
+        return Creator(obj);
+    }
+
+    private static AITool[] Creator<T>(T obj) where T : class
+    {
         return [
             .. typeof(T)
-                .GetMethods(BindingFlags.Static | BindingFlags.Public)
+                .GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.DeclaredOnly)
                 .Select((m) => AIFunctionFactory.Create(m, target: null)), // Get from type static methods
             .. obj.GetType()
-                .GetMethods(BindingFlags.Instance | BindingFlags.Public)
+                .GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly)
                 .Select((m) => AIFunctionFactory.Create(m, target: obj)) // Get from instance methods
-        ];
+                ];
     }
 }
